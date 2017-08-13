05ce98af8ffb442b7319b4297b2181ee73aee109
Takuma Homma <nagomimatcha@gmail.com>
2017-08-13 22:11:20 +0900

    Enable to check post/remove action on notification

--- app/src/main/java/com/mataku/scrobscrob/app/service/AppleMusicNotificationService.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/service/AppleMusicNotificationService.kt
@@ -1,10 +1,12 @@
 package com.mataku.scrobscrob.app.service
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
 import android.util.Log
 
-class AppleMusicNotificationService: NotificationListenerService() {
+class AppleMusicNotificationService : NotificationListenerService() {
     private val APPLE_PACKAGE_NAME = "com.apple.android.music"
 
     override fun onCreate() {
@@ -15,5 +17,31 @@ class AppleMusicNotificationService: NotificationListenerService() {
         } catch (e: PackageManager.NameNotFoundException) {
             Log.i("AppleMusicNotification", "Apple music is NOT installed!")
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    // ステータスバーに通知が更新される
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        super.onNotificationPosted(sbn)
+        Log.i("Posted", "Updated!")
+
+        var intent: Intent = Intent("AppleMusic")
+        intent.putExtra("message", "Yes!!!")
+        sendBroadcast(intent)
+
+        // TODO: 曲情報の取得処理を書く
+    }
+
+    // ステータスバーから通知が消去される
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        super.onNotificationRemoved(sbn)
+        Log.i("Posted", "Removed")
+
+        var intent: Intent = Intent("AppleMusic")
+        intent.putExtra("message", "Yes!!!")
+        sendBroadcast(intent)
     }
 }
