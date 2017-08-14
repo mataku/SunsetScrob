dc9140f901c27e0d4f82fbdb3856e667c5ba3f5d
Takuma Homma <nagomimatcha@gmail.com>
2017-08-14 16:24:56 +0900

    定数名を apple music のアプリだと分かるようにする

--- app/src/main/java/com/mataku/scrobscrob/app/service/AppleMusicNotificationService.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/service/AppleMusicNotificationService.kt
@@ -6,13 +6,14 @@ import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
 
+
 class AppleMusicNotificationService : NotificationListenerService() {
-    private val APPLE_PACKAGE_NAME = "com.apple.android.music"
+    private val APPLE_MUSIC_PACKAGE_NAME = "com.apple.android.music"
 
     override fun onCreate() {
         super.onCreate()
         try {
-            val appleMusicPackageInfo = packageManager.getPackageInfo(APPLE_PACKAGE_NAME, 0)
+            val appleMusicPackageInfo = packageManager.getPackageInfo(APPLE_MUSIC_PACKAGE_NAME, 0)
             Log.i("AppleMusicNotification", "Apple music is installed! (version: ${appleMusicPackageInfo.versionCode}")
         } catch (e: PackageManager.NameNotFoundException) {
             Log.i("AppleMusicNotification", "Apple music is NOT installed!")
@@ -26,19 +27,21 @@ class AppleMusicNotificationService : NotificationListenerService() {
     // ステータスバーに通知が更新される
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         super.onNotificationPosted(sbn)
-        Log.i("Posted", "Updated!")
 
+        // Ignore if not Apple Music APP
+        if (sbn.packageName != APPLE_MUSIC_PACKAGE_NAME) {
+            Log.i("Notification", "Not apple music notification")
+            return
+        }
+        
         var intent: Intent = Intent("AppleMusic")
         intent.putExtra("message", "Yes!!!")
         sendBroadcast(intent)
-
-        // TODO: 曲情報の取得処理を書く
     }
 
     // ステータスバーから通知が消去される
     override fun onNotificationRemoved(sbn: StatusBarNotification) {
         super.onNotificationRemoved(sbn)
-        Log.i("Posted", "Removed")
 
         var intent: Intent = Intent("AppleMusic")
         intent.putExtra("message", "Yes!!!")
