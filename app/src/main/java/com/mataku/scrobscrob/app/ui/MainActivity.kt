dd3cfdae920afcecbcc06e404ebb3ddb6dfee002
Takuma Homma <nagomimatcha@gmail.com>
2017-08-14 16:21:47 +0900

    Show settings if can not access to notification

--- app/src/main/java/com/mataku/scrobscrob/app/ui/MainActivity.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.mataku.scrobscrob.app.ui
 
+import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
 import com.mataku.scrobscrob.R
 import com.mataku.scrobscrob.app.service.AppleMusicNotificationReceiver
@@ -11,6 +13,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!isEnabledReadNotification()) {
+            showNotificationAccessSettingMenu()
+        }
         setContentView(R.layout.activity_main)
         val filter: IntentFilter = IntentFilter()
         filter.addAction("AppleMusic")
@@ -20,5 +25,28 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(receiver)
+    }
+
+    private fun showNotificationAccessSettingMenu() {
+        val intent = Intent()
+        intent.action = Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS
+        startActivity(intent)
+    }
+
+    private fun isEnabledReadNotification(): Boolean {
+        val contentResolver = contentResolver
+        val rawListeners = Settings.Secure.getString(contentResolver,
+                "enabled_notification_listeners")
+        if (rawListeners == null || "" == rawListeners) {
+            return false
+        } else {
+            val listeners = rawListeners.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            for (listener in listeners) {
+                if (listener.startsWith(packageName)) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
