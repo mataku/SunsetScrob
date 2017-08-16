3fbb50e7d34421d51ad192ab110d5ddd3fa51f80
Takuma Homma <nagomimatcha@gmail.com>
2017-08-16 18:28:41 +0900

    雑に LoginPresenter 作った

---
+++ app/src/main/java/com/mataku/scrobscrob/app/presenter/LoginPresenter.kt
@@ -0,0 +1,42 @@
+package com.mataku.scrobscrob.app.presenter
+
+import com.mataku.scrobscrob.BuildConfig
+import com.mataku.scrobscrob.app.model.api.Retrofit2LastFmClient
+import com.mataku.scrobscrob.app.ui.view.LoginViewCallback
+import java.math.BigInteger
+import java.security.MessageDigest
+
+class LoginPresenter(var view: LoginViewCallback) {
+    private val apiKey = BuildConfig.API_KEY
+    private val sharedSecret = BuildConfig.SHARED_SECRET
+    private val method = "auth.getMobileSession"
+    private val format = "json"
+
+    fun authenticate(userName: String, password: String) {
+        val apiSig: String = generateApiSig(userName, password)
+        val client = Retrofit2LastFmClient.createService()
+        val call = client.authenticate(userName, password, apiKey, apiSig, format)
+        try {
+            val response = call.execute()
+            val mobileSession = response?.body()?.mobileSession
+            view.setSessionKey(mobileSession!!.key)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun generateApiSig(userName: String, password: String): String {
+        val str = "api_key${apiKey}method${method}password${password}username${userName}${sharedSecret}"
+        var md5Str = ""
+        try {
+            var strBytes: ByteArray = str.toByteArray(charset("UTF-8"))
+            val md = MessageDigest.getInstance("MD5")
+            val md5Bytes = md.digest(strBytes)
+            val bigInt = BigInteger(1, md5Bytes)
+            md5Str = bigInt.toString(16)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return md5Str
+    }
+}
