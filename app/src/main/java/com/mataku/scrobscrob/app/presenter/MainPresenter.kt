0c20ef3d9d22bcf5db5f54b23987fb699e611578
Takuma Homma <nagomimatcha@gmail.com>
2017-08-16 18:36:29 +0900

    設定画面を表示するかどうかのみの責務にする

--- app/src/main/java/com/mataku/scrobscrob/app/presenter/MainPresenter.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/presenter/MainPresenter.kt
@@ -3,10 +3,8 @@ package com.mataku.scrobscrob.app.presenter
 import com.mataku.scrobscrob.app.ui.view.MainViewCallback
 
 class MainPresenter(var view: MainViewCallback) {
-    fun showPreparationMenuIfNeeded(userName: String, isEnabledReadNotification: Boolean) {
-        if (userName.isEmpty()) {
-            view.showLoginActivity()
-        } else if (isEnabledReadNotification) {
+    fun showPreparationMenuIfNeeded(isEnabledReadNotification: Boolean) {
+        if (!isEnabledReadNotification) {
             view.showNotificationAccessSettingMenu()
         }
     }
