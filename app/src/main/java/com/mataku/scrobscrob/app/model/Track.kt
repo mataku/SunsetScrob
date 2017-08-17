f0999e7d564fd97421df3bd67b67c36a9dc7c6e2
Takuma Homma <nagomimatcha@gmail.com>
2017-08-17 16:03:34 +0900

    メソッドが欲しくなったので Track model を data class から class に変更する

--- app/src/main/java/com/mataku/scrobscrob/app/model/Track.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/Track.kt
@@ -1,7 +1,10 @@
 package com.mataku.scrobscrob.app.model
 
-data class Track(
-        var artistName: String,
-        var name: String,
-        var albumName: String
-)
+import java.util.*
+
+class Track(artistName: String, trackName: String, albumName: String, startedAt: Date) {
+    var artistName: String = artistName
+    var trackName: String = trackName
+    var albumName: String = albumName
+    var startedAt: Date = startedAt
+}
