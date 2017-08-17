6c4df5a59213bb4e302d1367a4c8ac7138fa4402
Takuma Homma <nagomimatcha@gmail.com>
2017-08-17 11:45:33 +0900

    ログイン画面に関する presenter のメソッドの動作テスト

---
+++ app/src/test/java/com/mataku/scrobscrob/app/presenter/LoginPresenterTest.kt
@@ -0,0 +1,55 @@
+package com.mataku.scrobscrob.app.presenter
+
+import com.mataku.scrobscrob.app.ui.view.LoginViewCallback
+import org.junit.Test
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.test.assertTrue
+import kotlin.test.fail
+
+class LoginPresenterTest {
+    @Test
+    @Throws(Exception::class)
+    fun backToSettingsWhenLoggedIn_success() {
+        val showSuccessMessageCalled = AtomicBoolean()
+
+        class MockView : MockLoginView() {
+            override fun showSuccessMessage() {
+                showSuccessMessageCalled.set(true)
+            }
+        }
+
+        val presenter = LoginPresenter(MockView())
+        presenter.backToSettingsWhenLoggedIn(true, "someKey")
+        assertTrue(showSuccessMessageCalled.get())
+    }
+
+    @Test
+    @Throws
+    fun backToSettingsWhenLoggedIn_failure() {
+        val focusOnPasswordViewCalled = AtomicBoolean()
+
+        class MockView : MockLoginView() {
+            override fun focusOnPasswordView() {
+                focusOnPasswordViewCalled.set(true)
+            }
+        }
+
+        val presenter = LoginPresenter(MockView())
+        presenter.backToSettingsWhenLoggedIn(true, "")
+        assertTrue(focusOnPasswordViewCalled.get())
+    }
+
+    private open class MockLoginView : LoginViewCallback {
+        override fun showSuccessMessage() {
+            fail()
+        }
+
+        override fun setSessionKey(sessionKey: String) {
+            fail()
+        }
+
+        override fun focusOnPasswordView() {
+            fail()
+        }
+    }
+}
