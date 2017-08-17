b3da2161d70b0baa10772b6f2aea4eed383ce627
Takuma Homma <nagomimatcha@gmail.com>
2017-08-17 13:03:28 +0900

    ログイン画面の表示項目と遷移先を振り分ける判断をするための presenter 作成

---
+++ app/src/main/java/com/mataku/scrobscrob/app/presenter/SettingsPresenter.kt
@@ -0,0 +1,21 @@
+package com.mataku.scrobscrob.app.presenter
+
+import com.mataku.scrobscrob.app.ui.view.SettingsViewCallback
+
+class SettingsPresenter(var view: SettingsViewCallback) {
+    fun setMessageAccordingToUserStatus(loggedInUserName: String) {
+        if (loggedInUserName.isEmpty()) {
+            view.setMessageToLogIn()
+        } else {
+            view.setMessageToLogOut(loggedInUserName)
+        }
+    }
+
+    fun setDestinationAccordingToUserStatus(loggedInUserName: String) {
+        if (loggedInUserName.isEmpty()) {
+            view.setDestinationToMenuToLogIn()
+        } else {
+            view.setDestinationToMenuToLogOut()
+        }
+    }
+}
