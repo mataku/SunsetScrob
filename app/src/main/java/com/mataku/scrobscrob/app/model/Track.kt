fef89f282c61ca37377d8e1df6807d915660ceaa
Takuma Homma <nagomimatcha@gmail.com>
2017-08-18 13:26:56 +0900

    val -> var (Track)

--- app/src/main/java/com/mataku/scrobscrob/app/model/Track.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/Track.kt
@@ -1,8 +1,8 @@
 package com.mataku.scrobscrob.app.model
 
 class Track(artistName: String, name: String, albumName: String, playingTime: Long) {
-    val artistName: String = artistName
-    val name: String = name
-    val albumName: String = albumName
-    val playingTime: Long = playingTime
+    var artistName: String = artistName
+    var name: String = name
+    var albumName: String = albumName
+    var playingTime: Long = playingTime
 }
