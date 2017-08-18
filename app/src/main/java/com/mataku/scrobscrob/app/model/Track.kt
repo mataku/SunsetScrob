0dcbf42c806ee87b123a6ed4d6f8b2122170dace
Takuma Homma <nagomimatcha@gmail.com>
2017-08-18 14:26:04 +0900

    Use data class

--- app/src/main/java/com/mataku/scrobscrob/app/model/Track.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/Track.kt
@@ -1,8 +1,8 @@
 package com.mataku.scrobscrob.app.model
 
-class Track(artistName: String, name: String, albumName: String, playingTime: Long) {
-    var artistName: String = artistName
-    var name: String = name
-    var albumName: String = albumName
-    var playingTime: Long = playingTime
-}
+data class Track(
+        var artistName: String,
+        var name: String,
+        var albumName: String,
+        var playingTime: Long
+)
