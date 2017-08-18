ad6e981626e185e380c86d99de1f9172a84c7f6c
Takuma Homma <nagomimatcha@gmail.com>
2017-08-18 13:33:04 +0900

    let's get track info!!!

---
+++ app/src/main/java/com/mataku/scrobscrob/app/presenter/AppleMusicNotificationServicePresenter.kt
@@ -0,0 +1,49 @@
+package com.mataku.scrobscrob.app.presenter
+
+import android.util.Log
+import com.mataku.scrobscrob.app.model.Track
+import com.mataku.scrobscrob.app.model.api.Retrofit2LastFmClient
+import com.mataku.scrobscrob.app.model.entity.TrackInfoApiResponse
+import com.mataku.scrobscrob.app.ui.view.NotificationServiceInterface
+import com.mataku.scrobscrob.app.util.Settings
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class AppleMusicNotificationServicePresenter(var notificationServiceInterface: NotificationServiceInterface) {
+
+    private val settings = Settings()
+    private val apiKey = settings.apiKey
+
+    fun getTrackInfo(track: Track) {
+        val client = Retrofit2LastFmClient.createService()
+        val call = client.getTrackInfo(
+                track.artistName,
+                track.name,
+                apiKey
+        )
+
+        call.enqueue(object : Callback<TrackInfoApiResponse> {
+            override fun onResponse(call: Call<TrackInfoApiResponse>?, response: Response<TrackInfoApiResponse>?) {
+                if (response!!.isSuccessful) {
+                    if (response.body() != null) {
+                        val body = response.body()
+                        val playingTime = body?.trackInfo?.duration!!.toLong()
+                        track.playingTime = playingTime
+                        notificationServiceInterface.sendTrackInfoToReceiver(track)
+                        println(track.playingTime)
+                    } else {
+                        Log.i("TrackInfoApi", "No such song!")
+                        notificationServiceInterface.sendTrackInfoToReceiver(track)
+                    }
+                } else {
+                    Log.i("Notification", "Failed to get track info")
+                }
+            }
+
+            override fun onFailure(call: Call<TrackInfoApiResponse>?, t: Throwable?) {
+                Log.i("Notification", "Failure")
+            }
+        })
+    }
+}
