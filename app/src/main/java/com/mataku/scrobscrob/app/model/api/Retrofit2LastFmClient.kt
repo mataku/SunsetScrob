88bb972c0fe66bb03174f7901505e702008df360
Takuma Homma <nagomimatcha@gmail.com>
2017-08-19 00:03:38 +0900

    Enable logging for debug builds only

--- app/src/main/java/com/mataku/scrobscrob/app/model/api/Retrofit2LastFmClient.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/api/Retrofit2LastFmClient.kt
@@ -2,7 +2,10 @@ package com.mataku.scrobscrob.app.model.api
 
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
+import com.mataku.scrobscrob.BuildConfig
 import com.mataku.scrobscrob.app.model.api.service.LastFmService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,14 +13,28 @@ import retrofit2.converter.gson.GsonConverterFactory
 class Retrofit2LastFmClient {
     companion object {
         fun createService(): LastFmService {
+            val client = builderHttpClient()
+
             val apiUrl = "https://ws.audioscrobbler.com/"
             val gson = GsonBuilder().setLenient().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
             val retrofit = Retrofit.Builder()
                     .baseUrl(apiUrl)
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
                     .build()
             return retrofit.create(LastFmService::class.java)
+        }
+
+        private fun builderHttpClient(): OkHttpClient {
+            val client = OkHttpClient.Builder()
+            if (BuildConfig.DEBUG) {
+                val logging = HttpLoggingInterceptor()
+                logging.level = HttpLoggingInterceptor.Level.BODY
+                client.addInterceptor(logging)
+            }
+
+            return client.build()
         }
     }
 }
