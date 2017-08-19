61bb340e5b1646662b8ee3c20a44d3dd503bcf84
Takuma Homma <nagomimatcha@gmail.com>
2017-08-19 20:55:20 +0900

    Add ScrobbleListAdapter

---
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/adapter/ScrobbleListAdapter.kt
@@ -0,0 +1,23 @@
+package com.mataku.scrobscrob.app.ui.adapter
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.mataku.scrobscrob.app.model.Scrobble
+import com.mataku.scrobscrob.app.ui.view.ScrobbleView
+
+class ScrobbleListAdapter(private val context: Context) : BaseAdapter() {
+    var scrobbles = Scrobble().getAll()
+
+    override fun getCount(): Int = scrobbles.size
+
+    override fun getItem(position: Int): Any = scrobbles[position]
+
+    override fun getItemId(p0: Int): Long = 0
+
+    override fun getView(position: Int, view: View?, parent: ViewGroup?): View =
+            ((view as? ScrobbleView) ?: ScrobbleView(context)).apply {
+                setScrobble(scrobbles[position])
+            }
+}
