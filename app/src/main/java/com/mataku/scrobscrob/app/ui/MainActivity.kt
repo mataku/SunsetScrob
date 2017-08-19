bfc2ad1c052ac4bd52c9da9a268b321ac7fd1cb4
Takuma Homma <nagomimatcha@gmail.com>
2017-08-19 20:57:15 +0900

    Set adapter to ScrobbleListView

--- app/src/main/java/com/mataku/scrobscrob/app/ui/MainActivity.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/MainActivity.kt
@@ -9,10 +9,14 @@ import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ListView
 import com.mataku.scrobscrob.R
+import com.mataku.scrobscrob.app.model.Scrobble
 import com.mataku.scrobscrob.app.presenter.MainPresenter
 import com.mataku.scrobscrob.app.receiver.AppleMusicNotificationReceiver
+import com.mataku.scrobscrob.app.ui.adapter.ScrobbleListAdapter
 import com.mataku.scrobscrob.app.ui.view.MainViewCallback
+import io.realm.Realm
 
 class MainActivity : AppCompatActivity(), MainViewCallback {
     private var receiver = AppleMusicNotificationReceiver()
@@ -24,12 +28,19 @@ class MainActivity : AppCompatActivity(), MainViewCallback {
 //            sharedPreferences.edit().clear().apply()
 //        }
         super.onCreate(savedInstanceState)
+        Realm.init(this)
+        setContentView(R.layout.activity_main)
 //        mainPresenter = MainPresenter(this)
 //        mainPresenter.showPreparationMenuIfNeeded(isEnabledReadNotification())
 //        setContentView(R.layout.activity_main)
         val filter = IntentFilter()
         filter.addAction("AppleMusic")
         registerReceiver(receiver, filter)
+
+        val scrobbleListAdapter = ScrobbleListAdapter(applicationContext)
+        scrobbleListAdapter.scrobbles = dummyScrobbles()
+        var scrobbleListView = findViewById(R.id.list_view) as ListView
+        scrobbleListView.adapter = scrobbleListAdapter
     }
 
     override fun onDestroy() {
@@ -87,5 +98,17 @@ class MainActivity : AppCompatActivity(), MainViewCallback {
                     .forEach { return true }
         }
         return false
+    }
+
+    private fun dummyScrobbles(): List<Scrobble> {
+        var list = ArrayList<Scrobble>()
+
+        var scrobble = Scrobble()
+        scrobble.artistName = "PassCode"
+        scrobble.trackName = "insanity"
+        scrobble.albumName = "ZENITH"
+        list.add(scrobble)
+        list.add(scrobble)
+        return list
     }
 }
