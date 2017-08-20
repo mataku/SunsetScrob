1f08799bcf02e17414b42a48f1bf60bb313e620e
Takuma Homma <nagomimatcha@gmail.com>
2017-08-20 21:15:01 +0900

    Store previous track name to shared preferences

--- app/src/main/java/com/mataku/scrobscrob/app/service/AppleMusicNotificationService.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/service/AppleMusicNotificationService.kt
@@ -2,6 +2,7 @@ package com.mataku.scrobscrob.app.service
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
@@ -14,7 +15,7 @@ import com.mataku.scrobscrob.app.ui.view.NotificationServiceInterface
 class AppleMusicNotificationService : NotificationListenerService(), NotificationServiceInterface {
     private val APPLE_MUSIC_PACKAGE_NAME = "com.apple.android.music"
     private val presenter = AppleMusicNotificationServicePresenter(this)
-    private var previousTrackName: String = ""
+    private var track: Track? = null
 
     override fun onCreate() {
         super.onCreate()
@@ -35,6 +36,7 @@ class AppleMusicNotificationService : NotificationListenerService(), Notificatio
         super.onNotificationPosted(sbn)
         val sharedPreferences = getSharedPreferences("DATA", Context.MODE_PRIVATE)
         val sessionKey = sharedPreferences.getString("SessionKey", "")
+        val previousTrackName = sharedPreferences.getString("PreviousTrackName", "")
 
         // Ignore if not Apple Music APP
         if (sbn.packageName != APPLE_MUSIC_PACKAGE_NAME) {
@@ -55,6 +57,11 @@ class AppleMusicNotificationService : NotificationListenerService(), Notificatio
         extraTitle ?: return
         val trackName = extraTitle.toString()
 
+        if (previousTrackName == trackName) {
+            return
+        }
+
+        setPreviousTrackName(trackName)
         // artist name and album name
         // Format: android.text=artistName - albumName
         // e.g. PassCode — VIRTUAL
@@ -65,38 +72,39 @@ class AppleMusicNotificationService : NotificationListenerService(), Notificatio
         val artistName = array[0].trim()
         val albumName = array[1].trim()
         val timeStamp = System.currentTimeMillis() / 1000L
-        val track = Track(
+        track = Track(
                 artistName,
                 trackName,
                 albumName,
                 presenter.getTrackDuration(artistName, trackName),
                 timeStamp,
-                presenter.getAlbumArtWork(albumName, artistName, trackName)
+                ""
         )
-        sendTrackInfoToReceiver(track)
+        presenter.getAlbumArtWork(albumName, artistName, trackName)
     }
 
     // ステータスバーから通知が消去される
     override fun onNotificationRemoved(sbn: StatusBarNotification) {
         super.onNotificationRemoved(sbn)
+    }
 
+    override fun sendTrackInfoToReceiver(albumArtWork: String) {
+        Log.i("sendTrackInfo", "Called")
         val intent = Intent("AppleMusic")
+        intent.putExtra("artistName", track!!.artistName)
+        intent.putExtra("trackName", track!!.name)
+        intent.putExtra("albumName", track!!.albumName)
+        intent.putExtra("playingTime", track!!.playingTime)
+        intent.putExtra("timeStamp", track!!.timeStamp)
+        intent.putExtra("albumArtWork", albumArtWork)
+        println("Updated: ${intent.getStringExtra("albumArtWork")}")
         sendBroadcast(intent)
     }
 
-    private fun sendTrackInfoToReceiver(track: Track) {
-        if (previousTrackName == track.name) {
-            return
-        }
-
-        val intent = Intent("AppleMusic")
-        intent.putExtra("artistName", track.artistName)
-        intent.putExtra("trackName", track.name)
-        intent.putExtra("albumName", track.albumName)
-        intent.putExtra("playingTime", track.playingTime)
-        intent.putExtra("timeStamp", track.timeStamp)
-        intent.putExtra("albumArtWork", track.albumArtWork)
-        previousTrackName = track.name
-        sendBroadcast(intent)
+    private fun setPreviousTrackName(trackName: String) {
+        val data = getSharedPreferences("DATA", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = data.edit()
+        editor.putString("PreviousTrackName", trackName)
+        editor.apply()
     }
 }
