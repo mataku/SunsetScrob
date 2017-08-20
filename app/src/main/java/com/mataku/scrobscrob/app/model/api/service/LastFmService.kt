7d18a35bf70e40ff0121034947cd710686fcbdac
Takuma Homma <nagomimatcha@gmail.com>
2017-08-20 16:53:39 +0900

    Add api spec comment

--- app/src/main/java/com/mataku/scrobscrob/app/model/api/service/LastFmService.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/api/service/LastFmService.kt
@@ -9,6 +9,22 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface LastFmService {
+    @retrofit2.http.GET("/2.0/?method=track.getInfo&format=json")
+    fun getTrackInfo(
+            @Query("artist") artist: String,
+            @Query("track") track: String,
+            @Query("api_key") api_key: String
+    ): Call<TrackInfoApiResponse>
+
+    @retrofit2.http.GET("/2.0/?method=album.getInfo&format=json")
+    fun getAlbumInfo(
+            @Query("album") album: String,
+            @Query("artist") artist: String,
+            @Query("track") track: String,
+            @Query("api_key") api_key: String
+    ): Call<AlbumInfoApiResponse>
+
+    // Requests must be authenticated
     @retrofit2.http.POST("/2.0/?method=auth.getMobileSession&format=json")
     fun authenticate(
             @Query("username") userName: String,
@@ -27,21 +43,6 @@ interface LastFmService {
             @Field("api_sig") apiSig: String,
             @Field("sk") sessionKey: String
     ): Call<NowPlayingApiResponse>
-
-    @retrofit2.http.GET("/2.0/?method=track.getInfo&format=json")
-    fun getTrackInfo(
-            @Query("artist") artist: String,
-            @Query("track") track: String,
-            @Query("api_key") api_key: String
-    ): Call<TrackInfoApiResponse>
-
-    @retrofit2.http.GET("/2.0/?method=album.getInfo&format=json")
-    fun getAlbumInfo(
-            @Query("album") album: String,
-            @Query("artist") artist: String,
-            @Query("track") track: String,
-            @Query("api_key") api_key: String
-    ): Call<AlbumInfoApiResponse>
 
     // TODO: multi track scrobbling
     @FormUrlEncoded
