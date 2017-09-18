ddc9f5959fe889a90aa83527c2755a7cd8c30c24
Takuma Homma <nagomimatcha@gmail.com>
2017-09-18 22:45:59 +0900

    Create RealmMigration for Scrobble

---
+++ app/src/main/java/com/mataku/scrobscrob/app/data/Migration.kt
@@ -0,0 +1,22 @@
+package com.mataku.scrobscrob.app.data
+
+import io.realm.DynamicRealm
+import io.realm.RealmMigration
+import io.realm.RealmObjectSchema
+import io.realm.RealmSchema
+
+class Migration : RealmMigration {
+    override fun migrate(realm: DynamicRealm?, oldVersion: Long, newVersion: Long) {
+        val schema: RealmSchema = realm!!.schema
+        if (oldVersion == 0L) {
+            /*
+             *  Schema Version 0 to 1
+             *   removed timeStamp: Long
+             *
+             *  app Version <= 0.0.2
+             */
+            val scrobbleSchema: RealmObjectSchema = schema.get("Scrobble")
+            scrobbleSchema.removeField("timeStamp")
+        }
+    }
+}
