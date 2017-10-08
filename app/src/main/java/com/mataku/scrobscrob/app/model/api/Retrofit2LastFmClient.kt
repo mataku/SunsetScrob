5726880cca3931ec32ab2438fee5b9544a507e8d
Takuma Homma <nagomimatcha@gmail.com>
2017-10-08 21:41:32 +0900

    create でインスタンス作る

--- app/src/main/java/com/mataku/scrobscrob/app/model/api/Retrofit2LastFmClient.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/api/Retrofit2LastFmClient.kt
@@ -12,21 +12,6 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Retrofit2LastFmClient {
-    var retrofit: Retrofit
-
-    init {
-        val client = builderHttpClient()
-
-        val apiUrl = "https://ws.audioscrobbler.com/"
-        val gson = GsonBuilder().setLenient().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
-        retrofit = Retrofit.Builder()
-                .baseUrl(apiUrl)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-                .client(client)
-                .build()
-    }
-
     companion object {
         fun createService(): LastFmService {
             val client = builderHttpClient()
@@ -43,7 +28,17 @@ class Retrofit2LastFmClient {
         }
 
         fun <T> create(service: Class<T>): T {
-            return Retrofit2LastFmClient.create(service)
+            val client = builderHttpClient()
+
+            val apiUrl = "https://ws.audioscrobbler.com/"
+            val gson = GsonBuilder().setLenient().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+            return Retrofit.Builder()
+                    .baseUrl(apiUrl)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                    .client(client)
+                    .build()
+                    .create(service)
         }
 
         private fun builderHttpClient(): OkHttpClient {
