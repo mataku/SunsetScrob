f5e10fb953abfc0008217b0dcc36760899f1c4f3
Takuma Homma <nagomimatcha@gmail.com>
2017-10-08 20:32:57 +0900

    Create TrackScrobbleService

---
+++ app/src/main/java/com/mataku/scrobscrob/app/model/api/service/TrackScrobbleService.kt
@@ -0,0 +1,21 @@
+package com.mataku.scrobscrob.app.model.api.service
+
+import com.mataku.scrobscrob.app.model.entity.ScrobblesApiResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+
+interface TrackScrobbleService {
+    // TODO: multi track scrobbling
+    @FormUrlEncoded
+    @retrofit2.http.POST("/2.0/?method=track.scrobble&format=json")
+    fun scrobble(
+            @Field("artist[0]") artist: String,
+            @Field("track[0]") track: String,
+            @Field("timestamp[0]") timeStamp: Long,
+            @Field("album[0]") albumName: String,
+            @Field("api_key") apiKey: String,
+            @Field("api_sig") apiSig: String,
+            @Field("sk") sessionKey: String
+    ): Call<ScrobblesApiResponse>
+}
