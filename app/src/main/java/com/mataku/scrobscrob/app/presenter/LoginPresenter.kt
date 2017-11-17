aac439a20851eff18f33f3ee76e95f6718bfa32e
Takuma Homma <nagomimatcha@gmail.com>
2017-11-18 00:03:36 +0900

    Replace with await()

--- app/src/main/java/com/mataku/scrobscrob/app/presenter/LoginPresenter.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/presenter/LoginPresenter.kt
@@ -2,14 +2,19 @@ package com.mataku.scrobscrob.app.presenter
 
 import com.mataku.scrobscrob.app.model.api.Retrofit2LastFmClient
 import com.mataku.scrobscrob.app.model.api.service.AuthMobileSessionService
+import com.mataku.scrobscrob.app.model.entity.MobileSessionApiResponse
 import com.mataku.scrobscrob.app.ui.view.LoginViewCallback
 import com.mataku.scrobscrob.app.util.AppUtil
+import kotlinx.coroutines.experimental.runBlocking
+import retrofit2.HttpException
+import retrofit2.Response
+import ru.gildor.coroutines.retrofit.awaitResponse
 
 class LoginPresenter(var accessable: Boolean, var view: LoginViewCallback) {
     private val appUtil = AppUtil()
     private val method = "auth.getMobileSession"
 
-    fun authenticate(userName: String, password: String) {
+    fun authenticate(userName: String, password: String) = runBlocking {
         var params: MutableMap<String, String> = mutableMapOf()
         params["username"] = userName
         params["password"] = password
@@ -18,13 +23,16 @@ class LoginPresenter(var accessable: Boolean, var view: LoginViewCallback) {
 
         val apiSig: String = appUtil.generateApiSig(params)
         val client = Retrofit2LastFmClient.create(AuthMobileSessionService::class.java)
-        val call = client.authenticate(userName, password, appUtil.apiKey, apiSig)
         try {
-            val response = call.execute()
-            val mobileSession = response?.body()?.mobileSession
-            view.setSessionInfo(mobileSession!!.key, mobileSession.name)
-        } catch (e: Exception) {
-            e.printStackTrace()
+            val apiResponse: Response<MobileSessionApiResponse> = client.authenticate(userName, password, appUtil.apiKey, apiSig).awaitResponse()
+            if (apiResponse.isSuccessful) {
+                val mobileSession = apiResponse.body()!!.mobileSession
+                view.setSessionInfo(mobileSession.key, mobileSession.name)
+            }
+        } catch (e: HttpException) {
+            // e.printStackTrace()
+        } catch (e: Throwable) {
+            // e.printStackTrace()
         }
     }
 
