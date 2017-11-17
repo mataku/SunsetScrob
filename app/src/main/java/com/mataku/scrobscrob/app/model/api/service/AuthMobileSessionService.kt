4cfe1bf0b10f72dfc437e6a254a3b9a51a7c0e2a
Takuma Homma <nagomimatcha@gmail.com>
2017-11-18 00:01:48 +0900

    Refactor

--- app/src/main/java/com/mataku/scrobscrob/app/model/api/service/AuthMobileSessionService.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/api/service/AuthMobileSessionService.kt
@@ -2,10 +2,11 @@ package com.mataku.scrobscrob.app.model.api.service
 
 import com.mataku.scrobscrob.app.model.entity.MobileSessionApiResponse
 import retrofit2.Call
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface AuthMobileSessionService {
-    @retrofit2.http.POST("/2.0/?method=auth.getMobileSession&format=json")
+    @POST("/2.0/?method=auth.getMobileSession&format=json")
     fun authenticate(
             @Query("username") userName: String,
             @Query("password") password: String,
