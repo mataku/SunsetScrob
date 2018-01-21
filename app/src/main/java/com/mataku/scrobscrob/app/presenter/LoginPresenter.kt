85ab756e43227975eed308903a7bc2ee36b4ad20
Takuma Homma <nagomimatcha@gmail.com>
2018-01-21 18:28:22 +0900

    replace with kotlin coroutines

--- app/src/main/java/com/mataku/scrobscrob/app/presenter/LoginPresenter.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/presenter/LoginPresenter.kt
@@ -2,28 +2,36 @@ package com.mataku.scrobscrob.app.presenter
 
 import com.mataku.scrobscrob.app.model.api.Retrofit2LastFmClient
 import com.mataku.scrobscrob.app.model.api.service.AuthMobileSessionService
+import com.mataku.scrobscrob.app.model.entity.MobileSessionApiResponse
 import com.mataku.scrobscrob.app.ui.view.LoginViewCallback
 import com.mataku.scrobscrob.app.util.AppUtil
+import kotlinx.coroutines.experimental.runBlocking
+import ru.gildor.coroutines.retrofit.Result
+import ru.gildor.coroutines.retrofit.awaitResult
 
 class LoginPresenter(var accessable: Boolean, var view: LoginViewCallback) {
     private val appUtil = AppUtil()
     private val method = "auth.getMobileSession"
 
-    fun authenticate(userName: String, password: String) {
-        var params: MutableMap<String, String> = mutableMapOf()
+    fun authenticate(userName: String, password: String) = runBlocking {
+        val params: MutableMap<String, String> = mutableMapOf()
         params["username"] = userName
         params["password"] = password
         params["method"] = method
 
         val apiSig: String = appUtil.generateApiSig(params)
         val client = Retrofit2LastFmClient.create(AuthMobileSessionService::class.java)
-        val call = client.authenticate(userName, password, apiSig)
-        try {
-            val response = call.execute()
-            val mobileSession = response?.body()?.mobileSession
-            view.setSessionInfo(mobileSession!!.key, mobileSession.name)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        val result: Result<MobileSessionApiResponse> = client.authenticate(userName, password, apiSig).awaitResult()
+        when (result) {
+            is Result.Ok -> {
+                val mobileSession = result.value.mobileSession
+                view.setSessionInfo(mobileSession.key, mobileSession.name)
+            }
+            is Result.Error -> {
+            }
+            is Result.Exception -> {
+
+            }
         }
     }
 
