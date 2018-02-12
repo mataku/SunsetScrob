4210cee796ec85815a4da3da87dc0aa9bebca68b
Takuma Homma <nagomimatcha@gmail.com>
2018-02-13 00:40:05 +0900

    Album entity

---
+++ app/src/test/java/com/mataku/scrobscrob/app/model/entity/AlbumTest.kt
@@ -0,0 +1,18 @@
+package com.mataku.scrobscrob.app.model.entity
+
+import com.google.gson.Gson
+import org.junit.Test
+import kotlin.test.assertNotNull
+
+class AlbumInfoTest {
+    @Test
+    @Throws
+    fun testParsingJson() {
+        val gson = Gson()
+        val response = gson.fromJson(
+                TestUtils.getAssetFileString("album_get_info.json"),
+                AlbumInfoApiResponse::class.java
+        )
+        assertNotNull(response.albumInfo)
+    }
+}
