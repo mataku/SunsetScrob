6218cb8cde4c4ea0567f9b3bb8ac0df3c755c6da
Takuma Homma <nagomimatcha@gmail.com>
2018-02-25 22:49:44 +0900

    Create presenter to fetch user contents

---
+++ app/src/main/java/com/mataku/scrobscrob/app/presenter/UserContentPresenter.kt
@@ -0,0 +1,21 @@
+package com.mataku.scrobscrob.app.presenter
+
+import com.mataku.scrobscrob.app.model.api.Retrofit2LastFmClient
+import com.mataku.scrobscrob.app.model.api.service.UserTopAlbumsService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class UserContentPresenter {
+    fun getTopAlbums(userName: String) {
+
+        Retrofit2LastFmClient.create(UserTopAlbumsService::class.java)
+                .getTopAlbum(userName, "overall")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ result ->
+                    if (result.isSuccessful) {
+
+                    }
+                })
+    }
+}
