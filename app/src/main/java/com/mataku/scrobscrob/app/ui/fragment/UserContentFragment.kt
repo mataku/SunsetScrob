16985e508964554fab92009007ff8643e52314d6
Takuma Homma <nagomimatcha@gmail.com>
2018-02-25 22:48:59 +0900

    Create UserContentFragment

---
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/fragment/UserContentFragment.kt
@@ -0,0 +1,13 @@
+package com.mataku.scrobscrob.app.ui.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class UserContentFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
