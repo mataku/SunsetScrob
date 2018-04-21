b0f0b2778d69aed92e6a81b78be3bca440da6c4a
Takuma Homma <nagomimatcha@gmail.com>
2018-04-22 00:43:39 +0900

    import!

--- app/src/main/java/com/mataku/scrobscrob/app/model/api/service/AuthMobileSessionService.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/api/service/AuthMobileSessionService.kt
@@ -2,19 +2,12 @@ package com.mataku.scrobscrob.app.model.api.service
 
 import com.mataku.scrobscrob.app.model.entity.MobileSessionApiResponse
 import kotlinx.coroutines.experimental.Deferred
-import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface AuthMobileSessionService {
-    @retrofit2.http.POST("/2.0/?method=auth.getMobileSession&format=json")
-    fun authenticate(
-            @Query("username") userName: String,
-            @Query("password") password: String,
-            @Query("api_sig") apiSig: String
-    ): Call<MobileSessionApiResponse>
-
-    @retrofit2.http.POST("/2.0/?method=auth.getMobileSession&format=json")
+    @POST("/2.0/?method=auth.getMobileSession&format=json")
     fun auth(
             @Query("username") userName: String,
             @Query("password") password: String,
