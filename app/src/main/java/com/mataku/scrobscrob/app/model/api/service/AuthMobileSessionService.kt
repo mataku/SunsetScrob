649fc6a521f6de862ab3d4e54687f3dba64b441d
Takuma Homma <nagomimatcha@gmail.com>
2018-04-22 00:38:38 +0900

    Using Deferred type at AuthService

--- app/src/main/java/com/mataku/scrobscrob/app/model/api/service/AuthMobileSessionService.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/model/api/service/AuthMobileSessionService.kt
@@ -1,7 +1,9 @@
 package com.mataku.scrobscrob.app.model.api.service
 
 import com.mataku.scrobscrob.app.model.entity.MobileSessionApiResponse
+import kotlinx.coroutines.experimental.Deferred
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Query
 
 interface AuthMobileSessionService {
@@ -11,4 +13,12 @@ interface AuthMobileSessionService {
             @Query("password") password: String,
             @Query("api_sig") apiSig: String
     ): Call<MobileSessionApiResponse>
+
+    @retrofit2.http.POST("/2.0/?method=auth.getMobileSession&format=json")
+    fun auth(
+            @Query("username") userName: String,
+            @Query("password") password: String,
+            @Query("api_sig") apiSig: String
+    ): Deferred<Response<MobileSessionApiResponse>>
+
 }
