ac076eb6345165f1969ac388e6c0784183d1fbc1
Takuma Homma <nagomimatcha@gmail.com>
2018-04-22 00:38:09 +0900

    Add ApiClient with kotlin coroutines adapter

---
+++ app/src/main/java/com/mataku/scrobscrob/app/model/api/LastFmApiClient.kt
@@ -0,0 +1,40 @@
+package com.mataku.scrobscrob.app.model.api
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
+import com.mataku.scrobscrob.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class LastFmApiClient {
+    companion object {
+        fun <T> create(service: Class<T>): T {
+            val client = builderHttpClient()
+
+            val apiUrl = "https://ws.audioscrobbler.com/"
+            val gson = GsonBuilder().setLenient().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+            return Retrofit.Builder()
+                    .baseUrl(apiUrl)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                    .client(client)
+                    .build()
+                    .create(service)
+        }
+
+        private fun builderHttpClient(): OkHttpClient {
+            val client = OkHttpClient.Builder()
+            if (BuildConfig.DEBUG) {
+                val logging = HttpLoggingInterceptor()
+                logging.level = HttpLoggingInterceptor.Level.BODY
+                client.addInterceptor(logging)
+            }
+            client.addInterceptor(LastFmApiAuthInterceptor())
+
+            return client.build()
+        }
+    }
+}
