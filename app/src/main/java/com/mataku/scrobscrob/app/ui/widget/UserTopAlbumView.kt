9380c5d464e0f91640727288244cff17a50b2133
Takuma Homma <nagomimatcha@gmail.com>
2018-04-29 01:27:20 +0900

    Prepare views for UserContent

---
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/widget/UserTopAlbumView.kt
@@ -0,0 +1,69 @@
+package com.mataku.scrobscrob.app.ui.widget
+
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.LayoutInflater
+import com.airbnb.epoxy.ModelProp
+import com.airbnb.epoxy.ModelView
+import com.mataku.scrobscrob.R
+import com.mataku.scrobscrob.app.model.GlideApp
+import com.mataku.scrobscrob.app.model.entity.Album
+import com.mataku.scrobscrob.databinding.ModelTopAlbumViewBinding
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
+
+@ModelView(autoLayout = ModelView.Size.WRAP_WIDTH_WRAP_HEIGHT)
+class UserTopAlbumView : ConstraintLayout {
+
+    private lateinit var binding: ModelTopAlbumViewBinding
+
+    constructor(
+            context: Context?
+    ) : this(context, null)
+
+    constructor(
+            context: Context?, attrs: AttributeSet?
+    ) : this(context, attrs, 0)
+
+    constructor(
+            context: Context?, attrs: AttributeSet?, defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr) {
+        context ?: return
+        val inflater = LayoutInflater.from(context)
+        binding = DataBindingUtil.inflate(inflater, R.layout.model_top_album_view, this, true)
+//        val padding: Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8F, resources.displayMetrics).toInt()
+//        val imageSize = context.resources.displayMetrics.widthPixels / 2 - (padding * 2)
+//
+//
+//        val lp = binding.modelTopAlbumArtwork.layoutParams
+//        lp.width = imageSize
+//        lp.height = imageSize
+//        binding.modelTopAlbumArtwork.layoutParams = lp
+    }
+
+    @ModelProp
+    fun setAlbum(album: Album) {
+        binding.modelTopAlbumArtist.text = album.artist.name
+        binding.modelTopAlbumTrack.text = album.name
+        val image = album.imageList.last()
+
+        GlideApp.with(context)
+                .load(image.imageUrl)
+                .transform(RoundedCornersTransformation(45, 0, RoundedCornersTransformation.CornerType.BOTTOM))
+                .fitCenter()
+                .error(R.drawable.no_image)
+                .into(binding.modelTopAlbumArtwork)
+    }
+
+    @ModelProp
+    fun setImageSize(size: Int) {
+        val padding: Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8F, resources.displayMetrics).toInt()
+        val lp = binding.modelTopAlbumArtwork.layoutParams
+        val imageSize = size - (padding)
+        lp.width = imageSize
+        lp.height = imageSize
+        binding.modelTopAlbumArtwork.layoutParams = lp
+    }
+}
