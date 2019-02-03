1468a4e2684954ecad021833b2263b2c21d3b092
Takuma Homma <nagomimatcha@gmail.com>
2019-02-03 21:45:19 +0900

    Unused

--- app/src/main/java/com/mataku/scrobscrob/app/ui/top/album/TopAlbumContentViewCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.mataku.scrobscrob.app.ui.top.album
-
-import com.mataku.scrobscrob.app.model.entity.Album
-
-interface TopAlbumContentViewCallback {
-    fun show(albums: List<Album>)
-}
--- app/src/main/java/com/mataku/scrobscrob/app/ui/top/artist/TopArtistsContentViewCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.mataku.scrobscrob.app.ui.top.artist
-
-import com.mataku.scrobscrob.app.model.entity.Artist
-
-interface TopArtistsContentViewCallback {
-    fun show(artists: List<Artist>)
-}
