33193130e492d9569f80873ea09d5405cd81a7e9
Takuma Homma <nagomimatcha@gmail.com>
2019-02-08 01:31:41 +0900

    Generate repository by koin

--- app/src/main/java/com/mataku/scrobscrob/app/App.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/App.kt
@@ -1,6 +1,8 @@
 package com.mataku.scrobscrob.app
 
 import android.app.Application
+import com.mataku.scrobscrob.app.data.repository.TopAlbumsRepository
+import com.mataku.scrobscrob.app.data.repository.TopArtistsRepository
 import com.mataku.scrobscrob.app.ui.top.TopViewModel
 import org.koin.android.ext.android.startKoin
 import org.koin.androidx.viewmodel.ext.koin.viewModel
@@ -9,7 +11,9 @@ import org.koin.dsl.module.module
 class App : Application() {
 
     private val appModules = module {
-        viewModel { TopViewModel() }
+        single { TopArtistsRepository() }
+        single { TopAlbumsRepository() }
+        viewModel { TopViewModel(get(), get()) }
     }
 
     override fun onCreate() {
