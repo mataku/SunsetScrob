624282c2d898d03dd39cf06c37909ea5530d88a4
Takuma Homma <nagomimatcha@gmail.com>
2019-02-10 01:12:19 +0900

    Return Result.failure with network available message if IOException occurred

--- app/src/main/java/com/mataku/scrobscrob/app/data/repository/TopAlbumsRepository.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/data/repository/TopAlbumsRepository.kt
@@ -1,25 +1,30 @@
 package com.mataku.scrobscrob.app.data.repository
 
+import com.mataku.scrobscrob.R
 import com.mataku.scrobscrob.app.model.api.LastFmApiClient
 import com.mataku.scrobscrob.app.model.api.service.UserTopAlbumsService
 import com.mataku.scrobscrob.app.model.entity.Album
 import com.mataku.scrobscrob.app.model.entity.presentation.Result
+import java.io.IOException
 import javax.net.ssl.HttpsURLConnection
 
 class TopAlbumsRepository {
     suspend fun topAlbumsResponse(page: Int, userName: String): Result<List<Album>> {
-        val result = LastFmApiClient.create(UserTopAlbumsService::class.java)
-            .getTopAlbum(20, page, "overall", userName).await()
-        return when (result.code()) {
-            HttpsURLConnection.HTTP_OK -> {
-                result.body()?.topAlbums?.let {
-                    Result.success(it.albums)
-                } ?: Result.success(emptyList())
+        return try {
+            val result = LastFmApiClient.create(UserTopAlbumsService::class.java)
+                .getTopAlbum(20, page, "overall", userName).await()
+            when (result.code()) {
+                HttpsURLConnection.HTTP_OK -> {
+                    result.body()?.topAlbums?.let {
+                        Result.success(it.albums)
+                    } ?: Result.success(emptyList())
+                }
+                else -> {
+                    Result.failure(R.string.error_message_fetch_top_albums)
+                }
             }
-            else -> {
-                val error = Result.ErrorResponse(1, "Failed to get top albums")
-                Result.failure(error)
-            }
+        } catch (e: IOException) {
+            Result.failure(R.string.error_message_bad_network)
         }
     }
 }
--- app/src/main/java/com/mataku/scrobscrob/app/data/repository/TopArtistsRepository.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/data/repository/TopArtistsRepository.kt
@@ -1,25 +1,30 @@
 package com.mataku.scrobscrob.app.data.repository
 
+import com.mataku.scrobscrob.R
 import com.mataku.scrobscrob.app.model.api.LastFmApiClient
 import com.mataku.scrobscrob.app.model.api.service.UserTopArtistsService
 import com.mataku.scrobscrob.app.model.entity.Artist
 import com.mataku.scrobscrob.app.model.entity.presentation.Result
+import java.io.IOException
 import javax.net.ssl.HttpsURLConnection
 
 class TopArtistsRepository {
     suspend fun topArtistsResponse(page: Int, userName: String): Result<List<Artist>> {
-        val result = LastFmApiClient.create(UserTopArtistsService::class.java)
-            .getTopArtists(20, page, "overall", userName).await()
-        return when (result.code()) {
-            HttpsURLConnection.HTTP_OK -> {
-                result.body()?.topArtists?.let {
-                    Result.success(it.artists)
-                } ?: Result.success(emptyList())
+        return try {
+            val result = LastFmApiClient.create(UserTopArtistsService::class.java)
+                .getTopArtists(20, page, "overall", userName).await()
+            when (result.code()) {
+                HttpsURLConnection.HTTP_OK -> {
+                    result.body()?.topArtists?.let {
+                        Result.success(it.artists)
+                    } ?: Result.success(emptyList())
+                }
+                else -> {
+                    Result.failure(R.string.error_message_fetch_top_artists)
+                }
             }
-            else -> {
-                val error = Result.ErrorResponse(1, "Failed to get top albums")
-                Result.failure(error)
-            }
+        } catch (e: IOException) {
+            Result.failure(R.string.error_message_bad_network)
         }
     }
 }
