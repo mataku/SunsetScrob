a93f1585ae4749e23fabea8fc1f560036d57f2de
Takuma Homma <nagomimatcha@gmail.com>
2019-02-11 20:14:15 +0900

    Override onCreate with bundle

--- feature/licenses/src/main/java/com/mataku/scrobscrob/licenses/ui/LicensesActivity.kt
+++ feature/licenses/src/main/java/com/mataku/scrobscrob/licenses/ui/LicensesActivity.kt
@@ -1,17 +1,17 @@
 package com.mataku.scrobscrob.licenses.ui
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.mataku.scrobscrob.licenses.R
 import com.mataku.scrobscrob.licenses.databinding.ActivityLicensesBinding
 
 class LicensesActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         val binding: ActivityLicensesBinding = DataBindingUtil.setContentView(this, R.layout.activity_licenses)
-        binding.licenses.loadUrl("file:///android_asset/licenses.html")
+        val webView = binding.licenses
+        webView.loadUrl("file:///android_asset/licenses.html")
     }
 }
