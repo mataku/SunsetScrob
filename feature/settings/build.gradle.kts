dded9c87975a29702a99afe19302cc38d535f768
Takuma Homma <nagomimatcha@gmail.com>
2019-02-24 00:41:11 +0900

    Convert settings.gradle

---
+++ feature/settings/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Versions.compileSdkVersion)
+
+    dataBinding.isEnabled = true
+
+    defaultConfig {
+        minSdkVersion(Versions.minSdkVersion)
+        targetSdkVersion(Versions.targetSdkVersion)
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+}
+
+dependencies {
+    testImplementation(Deps.junit)
+}
+
+repositories {
+    google()
+    jcenter()
+    mavenCentral()
+    maven("https://maven.google.com")
+}
+
+kapt {
+    correctErrorTypes = true
+}
