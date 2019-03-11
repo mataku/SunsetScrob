3721ef467295f10f5db526d3fdd872f028b0c83b
Takuma Homma <nagomimatcha@gmail.com>
2019-03-11 23:26:33 +0900

    Spek sample class

--- app/src/test/java/com/mataku/scrobscrob/app/model/entity/ScrobblesTest.kt
+++ app/src/test/java/com/mataku/scrobscrob/app/model/entity/ScrobblesTest.kt
@@ -3,23 +3,38 @@ package com.mataku.scrobscrob.app.model.entity
 import com.mataku.scrobscrob.core.entity.ScrobblesApiResponse
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
 import org.junit.Test
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 import kotlin.test.assertNotNull
+import kotlin.test.fail
 
-class ScrobblesTest {
+@RunWith(JUnitPlatform::class)
+class ScrobblesTest : Spek({
 
-    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
-    @Test
-    @Throws
-    fun testParsingJson() {
-        val jsonAdapter = moshi.adapter<ScrobblesApiResponse>(ScrobblesApiResponse::class.java)
-        val response = jsonAdapter.fromJson(
-            TestUtils.getAssetFileString("scrobbles.json")
-        )
-        assertNotNull(response?.scrobbles)
-        val scrobbles = response?.scrobbles
-        assertNotNull(scrobbles?.attr)
-        assertNotNull(scrobbles?.scrobble)
+    describe("Model properties") {
+        beforeGroup {
+            Thread.currentThread().setUncaughtExceptionHandler { _, _ -> fail() }
+        }
+        afterGroup {}
+        context("current") {
+            it("way") {
+                val jsonAdapter = moshi.adapter<ScrobblesApiResponse>(ScrobblesApiResponse::class.java)
+                val response = jsonAdapter.fromJson(
+                    TestUtils.getAssetFileString("scrobbles.json")
+                )
+                assertNotNull(response?.scrobbles)
+                val scrobbles = response?.scrobbles
+                assertNotNull(scrobbles?.attr)
+                assertNotNull(scrobbles?.scrobble)
+            }
+        }
     }
-}
+})
