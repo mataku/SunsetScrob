a003cc472d1e9824f505c7e2a607297f99d5f367
Takuma Homma <nagomimatcha@gmail.com>
2019-03-11 23:37:04 +0900

    Prepare TestApp

--- app/src/main/java/com/mataku/scrobscrob/app/App.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/App.kt
@@ -8,7 +8,7 @@ import org.koin.android.ext.android.startKoin
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
-class App : Application() {
+open class App : Application() {
 
     private val appModules = module {
         single { TopArtistsRepository() }
