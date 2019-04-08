89bc54c0e4189c4d7efd9000a06711e7c6921ee1
Takuma Homma <nagomimatcha@gmail.com>
2019-04-08 17:40:45 +0900

    Request wrapper

--- core/src/main/java/com/mataku/scrobscrob/core/api/ApiClient.kt
+++ core/src/main/java/com/mataku/scrobscrob/core/api/ApiClient.kt
@@ -5,6 +5,7 @@ import com.mataku.scrobscrob.core.api.endpoint.Endpoint
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.android.Android
+import io.ktor.client.features.defaultRequest
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.features.logging.LogLevel
@@ -16,11 +17,16 @@ import io.ktor.client.request.request
 import kotlinx.serialization.json.Json
 
 object ApiClient {
+    const val BASE_URL = "https://ws.audioscrobbler.com"
+
     val client: HttpClient
         get() {
             val config: HttpClientConfig<*>.() -> Unit = {
                 install(JsonFeature) {
                     serializer = KotlinxSerializer(Json.nonstrict)
+                }
+                defaultRequest {
+                    parameter("api_key", BuildConfig.API_KEY)
                 }
                 if (BuildConfig.DEBUG) {
                     install(Logging) {
@@ -33,7 +39,7 @@ object ApiClient {
         }
 
     suspend inline fun <reified T> request(endpoint: Endpoint) {
-        client.request<String>(endpoint.path) {
+        client.request<T>(BASE_URL + endpoint.path) {
             method = endpoint.requestType
             if (endpoint.params.isNotEmpty()) {
                 endpoint.params.forEach { k, v ->
@@ -42,5 +48,4 @@ object ApiClient {
             }
         }
     }
-
 }
