73da3da6c5665911b85201568648cd9accedabe3
Takuma Homma <nagomimatcha@gmail.com>
2019-04-08 17:40:26 +0900

    Endpoint for top artists

--- core/src/main/java/com/mataku/scrobscrob/core/api/endpoint/TopArtistsEndpoint.kt
+++ core/src/main/java/com/mataku/scrobscrob/core/api/endpoint/TopArtistsEndpoint.kt
@@ -1,6 +1,8 @@
 package com.mataku.scrobscrob.core.api.endpoint
 
+import io.ktor.http.HttpMethod
+
 data class TopArtistsEndpoint(
-    override val path: String,
-    override val requestType: RequestType = RequestType.GET
+    override val path: String = "/2.0/?method=user.getTopArtists&format=json",
+    override val requestType: HttpMethod = HttpMethod.Get
 ) : Endpoint
