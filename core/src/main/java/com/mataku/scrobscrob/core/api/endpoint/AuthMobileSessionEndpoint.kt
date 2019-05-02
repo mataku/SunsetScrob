13a50e3829de2b994554233f916d8338d4fb8ad0
Takuma Homma <nagomimatcha@gmail.com>
2019-05-02 22:09:27 +0900

    Endpoint for login request

---
+++ core/src/main/java/com/mataku/scrobscrob/core/api/endpoint/AuthMobileSessionEndpoint.kt
@@ -0,0 +1,25 @@
+package com.mataku.scrobscrob.core.api.endpoint
+
+import io.ktor.http.HttpMethod
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+data class AuthMobileSessionEndpoint(
+    override val path: String = "/2.0/?method=auth.getMobileSession&format=json",
+    override val requestType: HttpMethod = HttpMethod.Post,
+    override val params: Map<String, Any>
+) : Endpoint
+
+@Serializable
+data class AuthMobileSessionApiResponse(
+    @SerialName("session")
+    val mobileSession: MobileSession?
+)
+
+@Serializable
+data class MobileSession(
+    @SerialName("name")
+    val name: String,
+    @SerialName("key")
+    val key: String
+)
