3a884535e650b12646c954d55fe8f53f5f8bd88c
Takuma Homma <nagomimatcha@gmail.com>
2019-05-02 21:52:24 +0900

    Now playing request via api client

---
+++ app/src/main/java/com/mataku/scrobscrob/app/data/repository/NowPlayingRepository.kt
@@ -0,0 +1,37 @@
+package com.mataku.scrobscrob.app.data.repository
+
+import com.mataku.scrobscrob.app.util.AppUtil
+import com.mataku.scrobscrob.core.api.ApiClient
+import com.mataku.scrobscrob.core.api.endpoint.NowPlaying
+import com.mataku.scrobscrob.core.api.endpoint.NowPlayingApiResponse
+import com.mataku.scrobscrob.core.api.endpoint.UpdateNowPlayingEndpoint
+import com.mataku.scrobscrob.core.entity.presentation.SunsetResult
+
+class NowPlayingRepository(private val apiClient: ApiClient) {
+    suspend fun update(trackName: String, artistName: String, sessionKey: String): SunsetResult<NowPlaying> {
+        val params = mutableMapOf(
+            "artist" to artistName,
+            "track" to trackName,
+            "album" to "",
+            "method" to NOW_PLAYING_METHOD,
+            "sk" to sessionKey
+        )
+        val apiSig = AppUtil.generateApiSig(params)
+        params.remove("method")
+        params["api_sig"] = apiSig
+
+        return try {
+            val request = apiClient.post<NowPlayingApiResponse>(UpdateNowPlayingEndpoint(params = params))
+            val nowPlaying = request.nowPlaying
+            nowPlaying?.let {
+                SunsetResult.success(it)
+            } ?: SunsetResult.failure(Throwable())
+        } catch (e: Exception) {
+            SunsetResult.failure(e)
+        }
+    }
+
+    companion object {
+        private const val NOW_PLAYING_METHOD = "track.updateNowPlaying"
+    }
+}
