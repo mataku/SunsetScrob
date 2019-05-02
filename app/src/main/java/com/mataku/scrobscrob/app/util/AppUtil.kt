11ff4cd6f8cbd40648bc77c5371c6457bd5273ab
Takuma Homma <nagomimatcha@gmail.com>
2019-05-02 21:36:37 +0900

    Only to generate signature

--- app/src/main/java/com/mataku/scrobscrob/app/util/AppUtil.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/util/AppUtil.kt
@@ -5,11 +5,7 @@ import com.mataku.scrobscrob.BuildConfig
 import java.security.MessageDigest
 
 class AppUtil {
-    val apiKey = BuildConfig.API_KEY
-    val sharedSecret = BuildConfig.SHARED_SECRET
     val defaultPlayingTime = 180.toLong()
-    val latestScrobbleCountToDisplay = 20
-    val topAlbumsCountPerPage = 20
 
     fun generateApiSig(params: MutableMap<String, String>): String {
         var str = ""
@@ -36,6 +32,35 @@ class AppUtil {
     fun debugLog(key: String, value: String?) {
         if (BuildConfig.DEBUG) {
             Log.d(key, value)
+        }
+    }
+
+    companion object {
+        const val apiKey = BuildConfig.API_KEY
+        const val sharedSecret = BuildConfig.SHARED_SECRET
+        const val latestScrobbleCountToDisplay = 20
+        const val topAlbumsCountPerPage = 20
+
+        fun generateApiSig(params: MutableMap<String, String>): String {
+            var str = ""
+            params["api_key"] = apiKey
+            params.toSortedMap().forEach { k, v ->
+                str += k + v
+            }
+            str += sharedSecret
+
+            val messageDigest = MessageDigest.getInstance("MD5")
+            val byteData = str.toByteArray()
+            messageDigest.update(byteData)
+            val digest = messageDigest.digest()
+            val stringBuilder = StringBuilder()
+            digest.indices.forEach { i ->
+                val b = (0xFF and digest[i].toInt())
+                if (b < 16)
+                    stringBuilder.append("0")
+                stringBuilder.append(Integer.toHexString(b))
+            }
+            return stringBuilder.toString()
         }
     }
 }
