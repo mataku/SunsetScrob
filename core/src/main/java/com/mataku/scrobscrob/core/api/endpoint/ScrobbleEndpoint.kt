e4ac19e23f5cc9ddb8719031df2642b988c1aee3
Takuma Homma <nagomimatcha@gmail.com>
2019-05-02 21:38:05 +0900

    Endpoint for scrobble request

---
+++ core/src/main/java/com/mataku/scrobscrob/core/api/endpoint/ScrobbleEndpoint.kt
@@ -0,0 +1,29 @@
+package com.mataku.scrobscrob.core.api.endpoint
+
+import io.ktor.http.HttpMethod
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+data class ScrobbleEndpoint(
+    override val path: String = "/2.0/?method=track.scrobble&format=json",
+    override val requestType: HttpMethod = HttpMethod.Post,
+    override val params: Map<String, Any>
+) : Endpoint
+
+@Serializable
+data class ScrobbleApiResponse(
+    val scrobbleResult: ScrobbleResult?
+)
+
+@Serializable
+data class ScrobbleResult(
+    val scrobbleAttr: ScrobbleAttr
+)
+
+@Serializable
+data class ScrobbleAttr(
+    @SerialName("accepted")
+    val accepted: Int?,
+    @SerialName("ignored")
+    val ignored: Int?
+)
