5ea59cb146d74c0fab1c433bea89e0524b314ecd
Takuma Homma <nagomimatcha@gmail.com>
2019-05-02 22:13:36 +0900

    Thanks, retrofit client

--- app/src/main/java/com/mataku/scrobscrob/app/App.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/App.kt
@@ -1,28 +1,17 @@
 package com.mataku.scrobscrob.app
 
 import android.app.Application
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.mataku.scrobscrob.BuildConfig
 import com.mataku.scrobscrob.app.data.repository.TopAlbumsRepository
 import com.mataku.scrobscrob.app.data.repository.TopArtistsRepository
-import com.mataku.scrobscrob.app.model.api.okhttp3.LastFmApiAuthInterceptor
 import com.mataku.scrobscrob.app.ui.top.TopViewModel
 import com.mataku.scrobscrob.core.api.ApiClient
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.android.startKoin
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 open class App : Application() {
 
     private val appModules = module {
-        single { okHttpClient() }
-        single { apiClient(get()) }
         single { TopArtistsRepository(get()) }
         single { TopAlbumsRepository(get()) }
         single { ApiClient }
@@ -35,28 +24,5 @@ open class App : Application() {
             this,
             listOf(appModules)
         )
-    }
-
-    fun okHttpClient(): OkHttpClient {
-        val client = OkHttpClient.Builder()
-        if (BuildConfig.DEBUG) {
-            val logging = HttpLoggingInterceptor()
-            logging.level = HttpLoggingInterceptor.Level.BODY
-            client.addInterceptor(logging)
-        }
-        client.addInterceptor(LastFmApiAuthInterceptor())
-
-        return client.build()
-    }
-
-    fun apiClient(okHttpClient: OkHttpClient): Retrofit {
-        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-
-        return Retrofit.Builder()
-            .baseUrl("https://ws.audioscrobbler.com/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .addCallAdapterFactory(CoroutineCallAdapterFactory())
-            .client(okHttpClient)
-            .build()
     }
 }
