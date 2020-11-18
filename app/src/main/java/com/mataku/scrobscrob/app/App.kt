0f9c08d03c5c416c2f744a63a1b9ee4373141050
Takuma Homma <nagomimatcha@gmail.com>
2020-11-19 03:05:40 +0900

    Setup flipper

--- app/src/main/java/com/mataku/scrobscrob/app/App.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/App.kt
@@ -2,12 +2,20 @@ package com.mataku.scrobscrob.app
 
 import android.app.Application
 import androidx.room.Room
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
+import com.mataku.scrobscrob.BuildConfig
 import com.mataku.scrobscrob.app.model.db.AppDatabase
 import com.mataku.scrobscrob.app.ui.top.TopViewModel
 import com.mataku.scrobscrob.core.api.LastFmApiClient
 import com.mataku.scrobscrob.core.api.repository.TopAlbumsRepository
 import com.mataku.scrobscrob.core.api.repository.TopArtistsRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -22,7 +30,7 @@ open class App : Application() {
     private val appModules = module {
         single { TopArtistsRepository(get()) }
         single { TopAlbumsRepository(get()) }
-        single { LastFmApiClient }
+        single { LastFmApiClient(this.androidContext()) }
         viewModel { TopViewModel(get(), get()) }
     }
 
@@ -31,7 +39,15 @@ open class App : Application() {
         database =
             Room.databaseBuilder(applicationContext, AppDatabase::class.java, "sunset_db").build()
         startKoin {
+            androidContext(this@App)
             modules(appModules)
+        }
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(NetworkFlipperPlugin())
+            client.start()
         }
     }
 }
