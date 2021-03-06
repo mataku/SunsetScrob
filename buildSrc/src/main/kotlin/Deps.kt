835b7db17a883ad674ce219771ca207334975138
Takuma Homma <nagomimatcha@gmail.com>
2021-03-07 02:00:10 +0900

    Update kotlin deps

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,7 +1,7 @@
-const val kotlinVersion = "1.3.72"
+const val kotlinVersion = "1.4.31"
 
 object Deps {
-    private const val ktorVersion = "1.2.2"
+    private const val ktorVersion = "1.5.1"
 
     const val appCompat = "androidx.appcompat:appcompat:1.1.0"
     const val androidGradlePlugin = "com.android.tools.build:gradle:4.1.1"
@@ -10,9 +10,8 @@ object Deps {
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
     const val crashlytics = "com.crashlytics.sdk.android:crashlytics:2.10.1"
 
-    const val epoxy = "com.airbnb.android:epoxy:3.9.0"
-    const val epoxyDatabinding = "com.airbnb.android:epoxy-databinding:3.9.0"
-    const val epoxyProcessor = "com.airbnb.android:epoxy-processor:3.9.0"
+    const val epoxy = "com.airbnb.android:epoxy:3.11.0"
+    const val epoxyProcessor = "com.airbnb.android:epoxy-processor:3.11.0"
 
     const val firebaseCore = "com.google.firebase:firebase-core:17.0.0"
 
@@ -25,9 +24,9 @@ object Deps {
     const val koinAndroidXScope = "org.koin:koin-androidx-scope:2.0.1"
     const val koinAndroidXViewModel = "org.koin:koin-androidx-viewmodel:2.0.1"
 
-    const val kotlinCoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.7"
+    const val kotlinCoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3"
     const val kotlinSerializationRuntime =
-        "org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0"
+        "org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0"
     const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
 
     const val ktorClientOkhttp = "io.ktor:ktor-client-okhttp:$ktorVersion"
@@ -46,8 +45,8 @@ object Deps {
     const val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"
     const val junitPlatformRunner = "org.junit.platform:junit-platform-runner:1.1.0"
 
-    const val roomRuntime = "androidx.room:room-runtime:2.0.0"
-    const val roomCompiler = "androidx.room:room-compiler:2.0.0"
+    const val roomRuntime = "androidx.room:room-runtime:2.2.6"
+    const val roomCompiler = "androidx.room:room-compiler:2.2.6"
 }
 
 object UnitTestDeps {
