54e78ee3d60c53e966c77f5f98a8f121b8aeadbc
Takuma Homma <nagomimatcha@gmail.com>
2021-12-05 15:41:45 +0900

    Add common textfield skeleton classes

---
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/organism/SensitiveTextField.kt
@@ -0,0 +1,24 @@
+package com.mataku.scrobscrob.ui_common.organism
+
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+
+@Composable
+fun SensitiveTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    imeAction: ImeAction = ImeAction.Done
+) {
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = imeAction
+        )
+    )
+}
