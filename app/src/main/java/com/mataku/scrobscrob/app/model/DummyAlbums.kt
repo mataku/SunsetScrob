1e5c9976ec7de8a89c4af872ca8d1ce5e35b6127
Takuma Homma <nagomimatcha@gmail.com>
2022-01-02 20:19:33 +0900

    Debuggable temporary

---
+++ app/src/main/java/com/mataku/scrobscrob/app/model/DummyAlbums.kt
@@ -0,0 +1,47 @@
+package com.mataku.scrobscrob.app.model
+
+import com.mataku.scrobscrob.core.api.endpoint.Album
+import com.mataku.scrobscrob.core.api.endpoint.AlbumArtist
+import com.mataku.scrobscrob.core.api.endpoint.Image
+
+object DummyAlbums {
+    val contents = listOf(
+        Album(
+            name = "生まれてから初めて見た夢",
+            artist = AlbumArtist(
+                name = "乃木坂46"
+            ),
+            imageList = listOf(
+                Image(
+                    imageUrl = "https://lastfm.freetls.fastly.net/i/u/300x300/115974ea870713fc196e5597626098b6.jpg"
+                )
+            ),
+            url = "https://example.com"
+        ),
+        Album(
+            name = "生まれてから初めて見た夢",
+            artist = AlbumArtist(
+                name = "乃木坂46"
+            ),
+            imageList = listOf(
+                Image(
+                    imageUrl = "https://lastfm.freetls.fastly.net/i/u/300x300/115974ea870713fc196e5597626098b6.jpg"
+                )
+            ),
+            url = "https://example.com"
+        ),
+        Album(
+            name = "生まれてから初めて見た夢",
+            artist = AlbumArtist(
+                name = "乃木坂46"
+            ),
+            imageList = listOf(
+                Image(
+                    imageUrl = "https://lastfm.freetls.fastly.net/i/u/300x300/115974ea870713fc196e5597626098b6.jpg"
+                )
+            ),
+            url = "https://example.com"
+        )
+    )
+
+}
