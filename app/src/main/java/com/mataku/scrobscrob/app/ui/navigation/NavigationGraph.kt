ee70ad5114e741fe2f449a268dbf034956ef904f
Takuma Homma <nagomimatcha@gmail.com>
2022-01-09 22:46:30 +0900

    Use navigation-animation

--- app/src/main/java/com/mataku/scrobscrob/app/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/navigation/NavigationGraph.kt
@@ -1,28 +1,62 @@
 package com.mataku.scrobscrob.app.ui.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.mataku.scrobscrob.app.ui.molecule.SunsetBottomNavItem
 import com.mataku.scrobscrob.app.ui.screen.ScrobbleScreen
 import com.mataku.scrobscrob.app.ui.screen.TopAlbumsScreen
 import com.mataku.scrobscrob.app.ui.screen.TopArtistsScreen
+import com.mataku.scrobscrob.ui_common.template.WebViewScreen
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun NavigationGraph(navController: NavHostController) {
-    NavHost(
+    AnimatedNavHost(
         navController = navController,
         startDestination = SunsetBottomNavItem.SCROBBLE.screenRoute
+//        enterTransition = {
+//            slideInHorizontally(initialOffsetX = { 1000 })
+//        },
+//        exitTransition = {
+//            val currentRoute = initialState.destination.route
+//            if (currentRoute?.startsWith("webview?url") == true) {
+//                slideOutVertically(targetOffsetY = { 0 })
+//            } else {
+//                slideOutHorizontally(targetOffsetX = { 1000 })
+//            }
+//        }
     ) {
-        composable(SunsetBottomNavItem.SCROBBLE.screenRoute) {
+        composable(
+            SunsetBottomNavItem.SCROBBLE.screenRoute
+        ) {
             ScrobbleScreen()
         }
-        composable(SunsetBottomNavItem.TOP_ALBUMS.screenRoute) {
-            TopAlbumsScreen()
+        composable(
+            SunsetBottomNavItem.TOP_ALBUMS.screenRoute
+        ) {
+            TopAlbumsScreen(navController)
         }
         composable(SunsetBottomNavItem.TOP_ARTISTS.screenRoute) {
             TopArtistsScreen()
+        }
+        composable(
+            "webview?url={url}",
+            arguments = listOf(navArgument("url") {
+                defaultValue = ""
+            }),
+            enterTransition = {
+                null
+            },
+            popExitTransition = {
+                slideOutVertically(targetOffsetY = { 1000 })
+            }
+        ) {
+            WebViewScreen(navController = navController, url = it.arguments?.getString("url")!!)
         }
     }
 }
--- app/src/main/java/com/mataku/scrobscrob/app/ui/screen/MainScreen.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/screen/MainScreen.kt
@@ -1,19 +1,27 @@
 package com.mataku.scrobscrob.app.ui.screen
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.mataku.scrobscrob.app.ui.molecule.SunsetBottomNavigation
 import com.mataku.scrobscrob.app.ui.navigation.NavigationGraph
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun MainScreen() {
-    val navController = rememberNavController()
+    val navController = rememberAnimatedNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
     Scaffold(
         bottomBar = {
-            SunsetBottomNavigation(navController = navController)
+            if (currentRoute?.startsWith("webview?url") == false) {
+                SunsetBottomNavigation(navController = navController)
+            }
         }
     ) {
-        NavigationGraph(navController = navController)
+        NavigationGraph(navController)
     }
 }
