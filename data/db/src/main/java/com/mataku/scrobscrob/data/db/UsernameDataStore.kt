4a4da308b06d063bfbc9ad896c3ec1122fa3d728
Takuma Homma <nagomimatcha@gmail.com>
2022-01-17 00:28:00 +0900

    Use SharedPref
    TODO: migrate to DataStore

--- data/db/src/main/java/com/mataku/scrobscrob/data/db/UsernameDataStore.kt
+++ data/db/src/main/java/com/mataku/scrobscrob/data/db/UsernameDataStore.kt
@@ -1,46 +1,51 @@
 package com.mataku.scrobscrob.data.db
 
 import android.content.Context
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
 import javax.inject.Singleton
 
-private val Context.dataStore by preferencesDataStore("DATA")
+//private val Context.dataStore by preferencesDataStore("DATA")
 
 @Singleton
 class UsernameDataStore(
     @ApplicationContext private val context: Context
 ) {
-    suspend fun username(): String? {
-        val preferences = context.dataStore.data.first()
-        return kotlin.runCatching {
-            preferences[USERNAME_KEY]
-        }.fold(
-            onSuccess = { it },
-            onFailure = {
-                null
-            }
-        )
+//    suspend fun username(): String? {
+//        val preferences = context.dataStore.data.first()
+//        return kotlin.runCatching {
+//            preferences[USERNAME_KEY]
+//        }.fold(
+//            onSuccess = { it },
+//            onFailure = {
+//                null
+//            }
+//        )
+//    }
+//
+//    suspend fun setUsername(username: String): Flow<Unit> {
+//        return flowOf(
+//            context.dataStore.edit {
+//                it[USERNAME_KEY] = username
+//            }
+//        ).flowOn(Dispatchers.IO)
+//            .map { }
+//    }
+
+    private val sharedPref = context.getSharedPreferences("DATA", Context.MODE_PRIVATE)
+
+    fun username(): String? {
+        return sharedPref.getString(USERNAME_KEY, null)
     }
 
-    suspend fun setUsername(username: String): Flow<Unit> {
-        return flowOf(
-            context.dataStore.edit {
-                it[USERNAME_KEY] = username
-            }
-        ).flowOn(Dispatchers.IO)
-            .map { }
+    fun setUsername(username: String) {
+        sharedPref.edit().putString(USERNAME_KEY, username).apply()
     }
+
+//    private companion object {
+//        val USERNAME_KEY = stringPreferencesKey("UserName")
+//    }
 
     private companion object {
-        val USERNAME_KEY = stringPreferencesKey("UserName")
+        const val USERNAME_KEY = "UserName"
     }
 }
