e7e9df66eac5c0db0a632d4e9d36fd7593608028
Takuma Homma <nagomimatcha@gmail.com>
2022-01-29 00:04:39 +0900

    Prepare a screen for login

--- app/src/main/java/com/mataku/scrobscrob/app/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/screen/LoginScreen.kt
@@ -3,12 +3,15 @@ package com.mataku.scrobscrob.app.ui.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
@@ -20,7 +23,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -28,7 +30,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.mataku.scrobscrob.R
 import com.mataku.scrobscrob.app.ui.viewmodel.LoginViewModel
@@ -61,60 +62,65 @@ fun LoginContent(
         modifier = Modifier
             .fillMaxSize()
             .background(Colors.ContentBackground)
-            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = stringResource(id = R.string.login_to_last_fm),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        OutlinedTextField(
+            value = idText,
+            onValueChange = {
+                idText = it
+            },
+            label = { Text(text = "Username or Email") },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = passwordText,
+            onValueChange = {
+                passwordText = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            label = { Text(text = "Password") },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Button(
+            onClick = {
+                onLoginButtonTap(idText, passwordText)
+            },
             modifier = Modifier
-                .align(alignment = Alignment.CenterHorizontally)
-                .weight(1F),
-            verticalArrangement = Arrangement.Center
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            enabled = !isLoading,
+            contentPadding = PaddingValues(vertical = 12.dp)
         ) {
-
-            Text(
-                text = stringResource(id = R.string.login_to_last_fm),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            OutlinedTextField(
-                value = idText,
-                onValueChange = {
-                    idText = it
-                },
-                label = { Text(text = "Username or Email") }
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            OutlinedTextField(
-                value = passwordText,
-                onValueChange = {
-                    passwordText = it
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                ),
-                label = { Text(text = "Password") }
-            )
+            Text(text = "Let me in!")
         }
-        Column() {
-            Button(
-                onClick = {
-                    onLoginButtonTap(idText, passwordText)
-                },
-                modifier = Modifier.fillMaxWidth(),
-                enabled = !isLoading
-            ) {
-                Text(text = "Login")
-            }
 
-            Spacer(modifier = Modifier.height(24.dp))
-        }
+        Spacer(modifier = Modifier.height(24.dp))
     }
+
 }
 
 @Preview(showBackground = true)
@@ -122,10 +128,9 @@ fun LoginContent(
 private fun LoginScreenPreview() {
     SunsetTheme {
         Surface {
-            LoginScreen(
-                navController = NavController(LocalContext.current),
-                viewModel = viewModel()
-            )
+            LoginContent(isLoading = false, onLoginButtonTap = { id, password ->
+
+            })
         }
     }
 }
