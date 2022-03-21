4f40bd5ad90929cba12e0da73dedccd1ecae5148
Takuma Homma <nagomimatcha@gmail.com>
2022-03-21 22:51:44 +0900

    Update env

--- buildSrc/src/main/kotlin/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -2,7 +2,6 @@ package dependency
 
 object Versions {
     const val compileSdkVersion = 31
-    const val minSdkVersion = 24
-    const val targetSdkVersion = 30
-    const val kotlin = "1.3.72"
+    const val minSdkVersion = 30
+    const val targetSdkVersion = 31
 }
