6ade8e46e06769b313bfb5cabbd5ca07b5d75462
Takuma Homma <nagomimatcha@gmail.com>
2022-05-22 02:15:04 +0900

    Add feature:scrobble module

---
+++ feature/scrobble/src/main/java/com/mataku/scrobscrob/scrobble/ui/state/ScrobbleScreenState.kt
@@ -0,0 +1,36 @@
+package com.mataku.scrobscrob.scrobble.ui.state
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.mataku.scrobscrob.scrobble.ui.viewmodel.ScrobbleViewModel
+
+class ScrobbleScreenState(
+    val navController: NavController,
+    private val viewModel: ScrobbleViewModel
+) {
+    val uiState = viewModel.uiState
+
+    fun onScrobbleTap(url: String) {
+        navController.navigate("webview?url=$url")
+    }
+
+    fun onScrollEnd() {
+        viewModel.fetchRecentTracks()
+    }
+}
+
+@Composable
+fun rememberScrobbleScreenState(
+    navController: NavController = rememberNavController(),
+    viewModel: ScrobbleViewModel = hiltViewModel()
+): ScrobbleScreenState {
+    return remember {
+        ScrobbleScreenState(
+            navController = navController,
+            viewModel = viewModel
+        )
+    }
+}
