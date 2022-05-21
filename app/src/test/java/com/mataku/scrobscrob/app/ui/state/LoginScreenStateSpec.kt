218973b0492b4a3a61e88ad1891fc404f1b5ca69
Takuma Homma <nagomimatcha@gmail.com>
2022-05-21 22:55:31 +0900

    Add specs for LoginScreenState

---
+++ app/src/test/java/com/mataku/scrobscrob/app/ui/state/LoginScreenStateSpec.kt
@@ -0,0 +1,82 @@
+package com.mataku.scrobscrob.app.ui.state
+
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavOptionsBuilder
+import com.mataku.scrobscrob.app.ui.molecule.SunsetBottomNavItem
+import com.mataku.scrobscrob.app.ui.viewmodel.LoginViewModel
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+class LoginScreenStateSpec : DescribeSpec({
+    val navController = mockk<NavController>()
+    val viewModel = mockk<LoginViewModel>()
+    beforeContainer {
+        every {
+            viewModel.uiState
+        }.returns(
+            LoginViewModel.UiState(
+                isLoading = false
+            )
+        )
+    }
+
+    val username = "username"
+    val password = "password"
+
+    describe("#login") {
+        val loginScreenState = LoginScreenState(
+            navController = navController,
+            viewModel = viewModel
+        )
+        it("calls authorize") {
+            coEvery {
+                viewModel.authorize(username = username, password = password)
+            }.returns(Unit)
+            loginScreenState.login(username, password)
+            coVerify(exactly = 1) {
+                viewModel.authorize(username = username, password = password)
+            }
+        }
+    }
+
+    describe("#navigateToTop") {
+        val loginScreenState = LoginScreenState(
+            navController = navController,
+            viewModel = viewModel
+        )
+        it("calls NavController.navigate") {
+            val navGraph = mockk<NavGraph>()
+            every {
+                navController.graph
+            }.returns(navGraph)
+            val navDestination = mockk<NavDestination>()
+            every {
+                navGraph.findStartDestination()
+            }.returns(navDestination)
+            every {
+                navDestination.id
+            }.returns(1)
+            val builder = mockk<NavOptionsBuilder>()
+            every {
+                navController.navigate(
+                    SunsetBottomNavItem.SCROBBLE.screenRoute,
+                    captureLambda()
+                )
+            }.answers { }
+            loginScreenState.navigateToTop()
+            verify(exactly = 1) {
+                navController.navigate(
+                    SunsetBottomNavItem.SCROBBLE.screenRoute,
+                    captureLambda()
+                )
+            }
+        }
+    }
+})
