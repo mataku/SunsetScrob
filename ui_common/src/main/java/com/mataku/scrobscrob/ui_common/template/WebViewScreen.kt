1a98dc41bd246b4d02e7df90b6530641f05ebe7e
Takuma Homma <nagomimatcha@gmail.com>
2022-05-24 21:56:39 +0900

    Wrap to accompanist-webview

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/template/WebViewScreen.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/template/WebViewScreen.kt
@@ -1,31 +1,46 @@
 package com.mataku.scrobscrob.ui_common.template
 
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
+import com.mataku.scrobscrob.ui_common.style.SunsetTheme
 
 @Composable
 fun WebViewScreen(navController: NavController, url: String) {
+    val state = rememberWebViewState(url = url)
     Column(
         modifier = Modifier
             .verticalScroll(rememberScrollState())
             .fillMaxSize()
     ) {
-        AndroidView(
-            factory = {
-                WebView(it)
-            },
-            update = { webView ->
-                webView.webViewClient = WebViewClient()
-                webView.loadUrl(url)
+        WebView(state = state)
+    }
+    if (state.isLoading) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingPreview() {
+    SunsetTheme {
+        Surface {
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator()
             }
-        )
+        }
     }
 }
