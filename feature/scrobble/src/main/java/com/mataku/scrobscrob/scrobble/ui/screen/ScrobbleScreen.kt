9e222517f95cab0e73db499b36c777841037e7af
Takuma Homma <nagomimatcha@gmail.com>
2022-07-31 17:49:32 +0900

    To control appBar background color, so handle it as header composable

--- feature/scrobble/src/main/java/com/mataku/scrobscrob/scrobble/ui/screen/ScrobbleScreen.kt
+++ feature/scrobble/src/main/java/com/mataku/scrobscrob/scrobble/ui/screen/ScrobbleScreen.kt
@@ -1,21 +1,27 @@
 package com.mataku.scrobscrob.scrobble.ui.screen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.mataku.scrobscrob.core.api.endpoint.RecentTrack
+import com.mataku.scrobscrob.scrobble.R
 import com.mataku.scrobscrob.scrobble.ui.molecule.Scrobble
-import com.mataku.scrobscrob.scrobble.ui.molecule.ScrobbleTopBar
 import com.mataku.scrobscrob.scrobble.ui.state.ScrobbleScreenState
 import com.mataku.scrobscrob.scrobble.ui.state.ScrobbleTopBarState
+import com.mataku.scrobscrob.ui_common.SunsetTextStyle
 import com.mataku.scrobscrob.ui_common.organism.InfiniteLoadingIndicator
 import com.mataku.scrobscrob.ui_common.style.Colors
 
@@ -28,7 +34,7 @@ fun ScrobbleScreen(
     val uiState = state.uiState
     Scaffold(
         topBar = {
-            ScrobbleTopBar(stateHolder = topBarState)
+//            ScrobbleTopBar(stateHolder = topBarState)
         },
         bottomBar = {}
     ) {
@@ -45,6 +51,7 @@ fun ScrobbleScreen(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun ScrobbleContent(
     recentTracks: List<RecentTrack>,
@@ -52,8 +59,24 @@ private fun ScrobbleContent(
     onScrobbleTap: (String) -> Unit,
     onScrollEnd: () -> Unit
 ) {
+
     LazyColumn(
         content = {
+            stickyHeader {
+                Text(
+                    text = stringResource(id = R.string.menu_scrobble),
+                    style = SunsetTextStyle.h6,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = Colors.ContentBackground
+                        )
+                        .padding(16.dp)
+                )
+            }
+
+
+
             items(recentTracks) {
                 Scrobble(
                     recentTrack = it,
