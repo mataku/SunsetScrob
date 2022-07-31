6901ec2af76f12ce91b66ab80e3ff8df70f4c125
Takuma Homma <nagomimatcha@gmail.com>
2022-08-01 00:51:40 +0900

    Add LightLime

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/style/Colors.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/style/Colors.kt
@@ -3,10 +3,11 @@ package com.mataku.scrobscrob.ui_common.style
 import androidx.compose.ui.graphics.Color
 
 object Colors {
-    val ContentBackground = Color(0xff263238)
-    val LastFmColor = Color(0xffbb0414)
-    val BarBackground = Color(0xff607D8B)
-    val textSecondary = Color(0xff90A4AE)
-    val Lime = Color(0xffCDDC39)
+    val ContentBackground = Color(0xFF263238)
+    val LastFmColor = Color(0xFFbb0414)
+    val BarBackground = Color(0xFF607D8B)
+    val textSecondary = Color(0xFF90A4AE)
+    val Lime = Color(0xFFCDDC39)
+    val LightLime = Color(0xFFC0CA33)
 }
 
