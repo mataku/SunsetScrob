c776cebbc756152537d1eb547e0054cb030e670d
Takuma Homma <nagomimatcha@gmail.com>
2022-07-31 17:51:15 +0900

    h6

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/SunsetTextStyle.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/SunsetTextStyle.kt
@@ -1,10 +1,12 @@
 package com.mataku.scrobscrob.ui_common
 
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.mataku.scrobscrob.ui_common.style.Colors
 
 object SunsetTextStyle {
+    // TODO: font
     val caption = TextStyle(
         fontSize = 12.sp,
         color = Colors.textSecondary
@@ -16,6 +18,12 @@ object SunsetTextStyle {
 
     val body2 = TextStyle(
         fontSize = 14.sp
+    )
+
+    val h6 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        letterSpacing = 0.15.sp
     )
 
 }
