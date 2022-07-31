6324de412fe76936454f5c654ac295c30e2099a3
Takuma Homma <nagomimatcha@gmail.com>
2022-08-01 00:55:58 +0900

    Add light theme

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/style/SunsetTheme.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/style/SunsetTheme.kt
@@ -2,31 +2,63 @@ package com.mataku.scrobscrob.ui_common.style
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.mataku.scrobscrob.core.entity.AppTheme
 
 @Composable
 fun SunsetTheme(
-    content: @Composable () -> Unit
+    theme: AppTheme = AppTheme.DARK,
+    content: @Composable () -> Unit,
 ) {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        theme.backgroundColor()
+    )
+    systemUiController.setNavigationBarColor(
+        theme.colors().primary
+    )
     MaterialTheme(
-        colors = darkColors,
+        colors = theme.colors(),
         content = content
     )
 }
 
-private val lightColors = lightColors(
-    
+fun AppTheme.backgroundColor(): Color {
+    return this.colors().background
+}
+
+fun AppTheme.colors(): androidx.compose.material.Colors {
+    return when (this) {
+        AppTheme.DARK -> {
+            darkColors
+        }
+        AppTheme.LIGHT -> {
+            lightColors
+        }
+    }
+}
+
+private val lightColors = darkColors(
+    primary = Color(0xFFE0E0E0),
+    onPrimary = Color.Black,
+    primaryVariant = Color.Black,
+    secondary = Color(0xFFFF4081),
+    onSecondary = Color.White,
+    surface = Color(0xFFF5F5F5),
+    onSurface = Color.Black,
+    background = Color(0xFFF5F5F5),
+    onBackground = Color.Black,
 )
 
 private val darkColors = darkColors(
-    primary = Color(0xff4c626d),
+    primary = Color.DarkGray,
     onPrimary = Color.White,
     primaryVariant = Colors.ContentBackground,
-    secondary = Color(0xffFF4081),
+    secondary = Color(0xFFFF4081),
     onSecondary = Color.White,
-    surface = Colors.ContentBackground,
+    surface = Color(0xFF37474F),
     onSurface = Color.White,
     background = Colors.ContentBackground,
     onBackground = Color.White
