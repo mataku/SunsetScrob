bbb58f1b9cf89cbf6944d344f5231d4d0dd1218b
Takuma Homma <nagomimatcha@gmail.com>
2022-08-04 21:53:00 +0900

    Set coil cache setting

--- app/src/main/java/com/mataku/scrobscrob/app/App.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/App.kt
@@ -1,7 +1,32 @@
 package com.mataku.scrobscrob.app
 
 import android.app.Application
+import coil.Coil
+import coil.ImageLoader
+import coil.disk.DiskCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
+import com.mataku.scrobscrob.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-open class App : Application()
+open class App : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    val imageLoaderBuilder = ImageLoader.Builder(applicationContext)
+      .memoryCachePolicy(CachePolicy.DISABLED)
+      .diskCachePolicy(CachePolicy.ENABLED)
+      .networkCachePolicy(CachePolicy.ENABLED)
+      .diskCache {
+        DiskCache.Builder()
+          .directory(cacheDir.resolve("sunsetscrob_image"))
+          .maxSizeBytes(256L * 1024L * 1024L)
+          .build()
+      }.crossfade(true)
+
+    if (BuildConfig.DEBUG) {
+      imageLoaderBuilder.logger(DebugLogger())
+    }
+    Coil.setImageLoader(imageLoaderBuilder.build())
+  }
+}
