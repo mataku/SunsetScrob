61276874a7a61535fc98d96907cbd423a7c9dd46
Takuma Homma <nagomimatcha@gmail.com>
2022-08-05 15:12:19 +0900

    Add components for TrackScreen

---
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/molecule/Chip.kt
@@ -0,0 +1,43 @@
+package com.mataku.scrobscrob.ui_common.molecule
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Chip(
+  name: String,
+  isSelected: Boolean = false,
+  toggleable: Boolean = true,
+  onSelectionChanged: (String) -> Unit = {},
+) {
+  Surface(
+    modifier = Modifier.padding(4.dp),
+    elevation = 8.dp,
+    shape = MaterialTheme.shapes.medium,
+    color = if (isSelected) Color.LightGray else MaterialTheme.colors.primary
+  ) {
+    Row(modifier = Modifier
+      .toggleable(
+        value = isSelected,
+        enabled = toggleable,
+        onValueChange = {
+          onSelectionChanged(name)
+        }
+      )
+    ) {
+      Text(
+        text = name,
+        style = MaterialTheme.typography.body2,
+        modifier = Modifier.padding(8.dp)
+      )
+    }
+  }
+}
