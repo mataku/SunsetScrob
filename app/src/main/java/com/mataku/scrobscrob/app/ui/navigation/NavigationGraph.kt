43b2f8e95ac7a62aeb7f5a6ca29d4ac350805627
Takuma Homma <nagomimatcha@gmail.com>
2022-08-06 15:41:36 +0900

    Use AnimatedNavHost to apply animated navigation

--- app/src/main/java/com/mataku/scrobscrob/app/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/navigation/NavigationGraph.kt
@@ -1,13 +1,16 @@
 package com.mataku.scrobscrob.app.ui.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.mataku.scrobscrob.account.ui.ThemeSelectorScreen
 import com.mataku.scrobscrob.account.ui.screen.AccountScreen
 import com.mataku.scrobscrob.account.ui.screen.LicenseScreen
@@ -21,27 +24,31 @@ import com.mataku.scrobscrob.artist.ui.state.rememberTopArtistsScreenState
 import com.mataku.scrobscrob.auth.ui.screen.LoginScreen
 import com.mataku.scrobscrob.auth.ui.state.rememberLoginScreenState
 import com.mataku.scrobscrob.scrobble.ui.screen.ScrobbleScreen
+import com.mataku.scrobscrob.scrobble.ui.screen.TrackScreen
 import com.mataku.scrobscrob.scrobble.ui.state.rememberScrobbleScreenState
 import com.mataku.scrobscrob.scrobble.ui.state.rememberTrackScreenState
 import com.mataku.scrobscrob.ui_common.SunsetBottomNavItem
+import com.mataku.scrobscrob.ui_common.style.ANIMATION_DURATION_MILLIS
 import com.mataku.scrobscrob.ui_common.template.WebViewScreen
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun NavigationGraph(
   navController: NavHostController,
-  isLoggedIn: Boolean,
-  scaffoldState: ScaffoldState
+  isLoggedIn: Boolean
 ) {
-  NavHost(
+  AnimatedNavHost(
     navController = navController,
     startDestination = if (isLoggedIn) SunsetBottomNavItem.SCROBBLE.screenRoute else "login"
   ) {
     composable(
-      SunsetBottomNavItem.SCROBBLE.screenRoute
+      SunsetBottomNavItem.SCROBBLE.screenRoute,
+      enterTransition = {
+        fadeIn(tween(ANIMATION_DURATION_MILLIS))
+      }
     ) {
       ScrobbleScreen(
-        state = rememberScrobbleScreenState(navController = navController),
-        trackScreenState = rememberTrackScreenState()
+        state = rememberScrobbleScreenState(navController = navController)
       )
     }
     composable(
@@ -59,6 +66,41 @@ fun NavigationGraph(
     composable(SunsetBottomNavItem.ACCOUNT.screenRoute) {
       AccountScreen(state = rememberAccountState(navController = navController))
     }
+    composable(
+      "track_detail?trackName={trackName}&artistName={artistName}&imageUrl={imageUrl}&upperLeftCoordinatorX={x}&upperLeftCoordinatorY={y}",
+      arguments = listOf(
+        navArgument("trackName") {
+          type = NavType.StringType
+        },
+        navArgument("artistName") {
+          type = NavType.StringType
+        },
+        navArgument("imageUrl") {
+          type = NavType.StringType
+        },
+        navArgument("x") {
+          type = NavType.IntType
+        },
+        navArgument("y") {
+          type = NavType.IntType
+        }
+      ),
+      content = {
+        val arguments = it.arguments ?: return@composable
+        val x = arguments.getInt("x", 0)
+        val y = arguments.getInt("y", 0)
+
+        TrackScreen(
+          trackName = arguments.getString("trackName", ""),
+          artistName = arguments.getString("artistName", ""),
+          artworkUrl = arguments.getString("imageUrl", ""),
+          topLeftCoordinate = Pair(x, y),
+          screenState = rememberTrackScreenState(navController),
+          onBackPressed = { }) {
+        }
+      }
+    )
+
     composable(
       "webview?url={url}",
       arguments = listOf(navArgument("url") {
