3ef422f5a692aa601af116405246bdd59e6c5d1d
Takuma Homma <nagomimatcha@gmail.com>
2022-08-08 23:57:08 +0900

    Modest

--- data/db/src/main/java/com/mataku/scrobscrob/data/db/AppDatabase.kt
+++ data/db/src/main/java/com/mataku/scrobscrob/data/db/AppDatabase.kt
@@ -3,7 +3,13 @@ package com.mataku.scrobscrob.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Scrobble::class], version = 1, exportSchema = false)
+@Database(
+  entities = [Scrobble::class, NowPlayingTrackEntity::class],
+  version = 1,
+  exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
   abstract val scrobbleDao: ScrobbleDao
+
+  abstract val nowPlayingDao: NowPlayingDao
 }
