ffd8b4f2ffd9385022f4667b62f0a234b8065aa2
Takuma Homma <nagomimatcha@gmail.com>
2022-08-08 23:55:49 +0900

    Store NowPlaying track

--- data/repository/src/main/java/com/mataku/scrobscrob/data/repository/NowPlayingRepository.kt
+++ data/repository/src/main/java/com/mataku/scrobscrob/data/repository/NowPlayingRepository.kt
@@ -1,50 +1,68 @@
 package com.mataku.scrobscrob.data.repository
 
-import com.mataku.scrobscrob.core.entity.NowPlaying
+import com.mataku.scrobscrob.core.entity.NowPlayingTrack
+import com.mataku.scrobscrob.core.entity.TrackInfo
+import com.mataku.scrobscrob.data.api.LastFmService
 import com.mataku.scrobscrob.data.api.endpoint.UpdateNowPlayingEndpoint
+import com.mataku.scrobscrob.data.db.NowPlayingDao
 import com.mataku.scrobscrob.data.db.SessionKeyDataStore
-import com.mataku.scrobscrob.data.repository.mapper.toNowPlaying
+import com.mataku.scrobscrob.data.repository.mapper.toNowPlayingTrack
+import com.mataku.scrobscrob.data.repository.mapper.toNowPlayingTrackEntity
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface NowPlayingRepository {
-  suspend fun update(trackName: String, artistName: String): Flow<NowPlaying>
+  suspend fun update(trackInfo: TrackInfo): Flow<Unit>
+
+  suspend fun current(): NowPlayingTrack?
 }
 
 @Singleton
 class NowPlayingRepositoryImpl @Inject constructor(
-  private val lastFmService: com.mataku.scrobscrob.data.api.LastFmService,
-  private val sessionKeyDataStore: SessionKeyDataStore
+  private val lastFmService: LastFmService,
+  private val sessionKeyDataStore: SessionKeyDataStore,
+  private val nowPlayingDao: NowPlayingDao
 ) : NowPlayingRepository {
-  override suspend fun update(
-    trackName: String,
-    artistName: String
-  ): Flow<NowPlaying> {
-    val sessionKey = sessionKeyDataStore.sessionKey()
-    return flow {
-      if (sessionKey == null) {
-        emptyFlow<NowPlaying>()
-      } else {
-        val params = mutableMapOf(
-          "artist" to artistName,
-          "track" to trackName,
-          "album" to "",
-          "method" to NOW_PLAYING_METHOD,
-          "sk" to sessionKey
-        )
-        val apiSig = ApiSignature.generateApiSig(params)
-        params.remove("method")
-        params["api_sig"] = apiSig
-        val endpoint = UpdateNowPlayingEndpoint(
-          params = params
-        )
-        val response = lastFmService.request(endpoint)
 
-        emit(response.toNowPlaying())
-      }
+  override suspend fun current(): NowPlayingTrack? =
+    nowPlayingDao.nowPlaying()?.toNowPlayingTrack()
+
+  override suspend fun update(trackInfo: TrackInfo) = flow {
+    val entity = trackInfo.toNowPlayingTrackEntity()
+    nowPlayingDao.insert(entity)
+    updateNowPlaying(
+      trackName = trackInfo.name,
+      artistName = trackInfo.artist.name,
+      albumName = trackInfo.album?.title ?: ""
+    )
+    emit(Unit)
+  }
+
+  private suspend fun updateNowPlaying(
+    trackName: String,
+    artistName: String,
+    albumName: String
+  ) {
+    val sessionKey = sessionKeyDataStore.sessionKey()
+    if (sessionKey == null) {
+      return
+    } else {
+      val params = mutableMapOf(
+        "artist" to artistName,
+        "track" to trackName,
+        "album" to albumName,
+        "method" to NOW_PLAYING_METHOD,
+        "sk" to sessionKey
+      )
+      val apiSig = ApiSignature.generateApiSig(params)
+      params.remove("method")
+      params["api_sig"] = apiSig
+      val endpoint = UpdateNowPlayingEndpoint(
+        params = params
+      )
+      lastFmService.request(endpoint)
     }
   }
 
