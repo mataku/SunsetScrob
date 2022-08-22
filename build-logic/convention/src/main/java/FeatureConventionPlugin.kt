be0c22f86562402cf9f497dc94be746714e3d886
Takuma Homma <nagomimatcha@gmail.com>
2022-08-23 00:43:06 +0900

    Create build logic for feature module

---
+++ build-logic/convention/src/main/java/FeatureConventionPlugin.kt
@@ -0,0 +1,29 @@
+import com.android.build.gradle.LibraryExtension
+import ext.kotlinConfiguration
+import ext.testConfiguration
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class FeatureConventionPlugin : Plugin<Project> {
+  override fun apply(target: Project) {
+    with(target) {
+      with(pluginManager) {
+        apply("com.android.library")
+        apply("org.jetbrains.kotlin.android")
+        apply("org.jetbrains.kotlin.kapt")
+
+        extensions.configure<LibraryExtension> {
+          kotlinConfiguration()
+          testConfiguration()
+          buildTypes {
+            getByName("debug") {
+            }
+            release {
+            }
+          }
+        }
+      }
+    }
+  }
+}
