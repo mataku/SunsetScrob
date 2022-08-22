1692735b555d82fdede84731771ba3cb93bd6ccb
Takuma Homma <nagomimatcha@gmail.com>
2022-08-23 01:44:59 +0900

    :shower: Unused
    Fix opt-in usage warnings

--- build-logic/convention/src/main/java/ext/KotlinConfiguration.kt
+++ build-logic/convention/src/main/java/ext/KotlinConfiguration.kt
@@ -2,7 +2,6 @@ package ext
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
-import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
 
@@ -19,25 +18,5 @@ fun CommonExtension<*, *, *, *>.kotlinConfiguration() {
   (this as ExtensionAware).extensions.configure<KotlinJvmOptions>("kotlinOptions") {
     jvmTarget = "1.8"
     freeCompilerArgs = freeCompilerArgs + listOf("-opt-in=kotlin.RequiresOptIn")
-  }
-}
-
-fun Project.kotlinConfiguration(
-  commonExtension: CommonExtension<*, *, *, *>
-) {
-  commonExtension.apply {
-    compileSdk = 33
-    defaultConfig {
-      minSdk = 29
-    }
-
-    compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_1_8
-      targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    (this as ExtensionAware).extensions.configure<KotlinJvmOptions>("kotlinOptions") {
-      jvmTarget = "1.8"
-      freeCompilerArgs = freeCompilerArgs + listOf("-opt-in=kotlin.RequiresOptIn")
-    }
   }
 }
