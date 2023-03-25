d3284f62ac5bf79b59dfb9fb1545b8dae10f112d
Takuma Homma <nagomimatcha@gmail.com>
2023-03-26 03:15:38 +0900

    Add koin module verification test

--- app/src/test/java/com/mataku/scrobscrob/app/di/AppModuleSpec.kt
+++ app/src/test/java/com/mataku/scrobscrob/app/di/AppModuleSpec.kt
@@ -1,19 +1,39 @@
 package com.mataku.scrobscrob.app.di
 
 import android.content.Context
+import com.mataku.scrobscrob.account.di.accountModule
+import com.mataku.scrobscrob.album.di.albumModule
+import com.mataku.scrobscrob.artist.di.artistModule
+import com.mataku.scrobscrob.auth.di.authModule
+import com.mataku.scrobscrob.data.repository.di.repositoryModule
+import com.mataku.scrobscrob.scrobble.di.scrobbleModule
 import io.kotest.core.spec.style.DescribeSpec
-import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngine
 import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.dsl.module
 import org.koin.test.verify.verify
 
 class AppModuleSpec : DescribeSpec({
   @OptIn(KoinExperimentalAPI::class)
   describe("verify") {
     it("should pass") {
-      appModule.verify(
+      val modules = listOf(
+        appModule,
+        repositoryModule, // includes api module and database module
+        accountModule,
+        albumModule,
+        artistModule,
+        authModule,
+        scrobbleModule
+      )
+      module {
+        includes(modules)
+      }.verify(
         extraTypes = listOf(
-          HttpClient::class,
-          Context::class
+          Context::class,
+          HttpClientEngine::class,
+          HttpClientConfig::class
         )
       )
     }
