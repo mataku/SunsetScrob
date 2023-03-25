996004ca9668f77a1359b32c76dd8a7f96570e04
Takuma Homma <nagomimatcha@gmail.com>
2023-03-25 23:07:06 +0900

    Prepare accountModule

--- feature/account/src/main/java/com/mataku/scrobscrob/account/di/AccountModule.kt
+++ feature/account/src/main/java/com/mataku/scrobscrob/account/di/AccountModule.kt
@@ -3,11 +3,16 @@ package com.mataku.scrobscrob.account.di
 import android.content.Context
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.mataku.scrobscrob.account.ui.viewmodel.AccountViewModel
+import com.mataku.scrobscrob.account.ui.viewmodel.ScrobbleSettingViewModel
+import com.mataku.scrobscrob.account.ui.viewmodel.ThemeSelectorViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
 import javax.inject.Singleton
 
 @Module
@@ -17,5 +22,32 @@ class AccountModule {
   @Provides
   fun provideAppUpdateManager(@ApplicationContext context: Context): AppUpdateManager {
     return AppUpdateManagerFactory.create(context)
+  }
+}
+
+val accountModule = module {
+  single<AppUpdateManager> {
+    AppUpdateManagerFactory.create(get())
+  }
+
+  viewModel {
+    AccountViewModel(
+      get(),
+      get(),
+      get(),
+      get()
+    )
+  }
+
+  viewModel {
+    ScrobbleSettingViewModel(
+      get()
+    )
+  }
+
+  viewModel {
+    ThemeSelectorViewModel(
+      get()
+    )
   }
 }
