11296786e3a4a7103af9d90795d4b6f2fc71ba56
Takuma Homma <nagomimatcha@gmail.com>
2023-03-26 23:42:15 +0900

    Use JDK 11 in subprojects for kapt compole option JDK usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,3 +24,11 @@ allprojects {
     mavenCentral()
   }
 }
+
+subprojects {
+  tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "11"
+    }
+  }
+}
