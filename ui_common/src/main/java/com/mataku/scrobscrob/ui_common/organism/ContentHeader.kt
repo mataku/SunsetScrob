7e9eab4c3f044e9f9e41cda3c98ff39d8b26ba5e
Takuma Homma <nagomimatcha@gmail.com>
2023-08-12 20:53:43 +0900

    Add ContentHeader previews

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/organism/ContentHeader.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/organism/ContentHeader.kt
@@ -9,8 +9,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.airbnb.android.showkase.annotation.ShowkaseComposable
+import com.mataku.scrobscrob.core.entity.AppTheme
 import com.mataku.scrobscrob.ui_common.SunsetTextStyle
+import com.mataku.scrobscrob.ui_common.style.SunsetThemePreview
 
 @Composable
 fun ContentHeader(
@@ -33,5 +37,46 @@ fun ContentHeader(
           .padding(16.dp)
       )
     }
+  }
+}
+
+@Preview
+@ShowkaseComposable(
+  name = "ContentHeader",
+  group = "Common",
+  defaultStyle = true,
+  styleName = "Dark"
+)
+@Composable
+fun ContentHeaderPreview() {
+  SunsetThemePreview {
+    ContentHeader(text = "Scrobble")
+  }
+}
+
+@Preview
+@ShowkaseComposable(name = "ContentHeader", group = "Common", styleName = "Light")
+@Composable
+fun ContentHeaderLightPreview() {
+  SunsetThemePreview(theme = AppTheme.LIGHT) {
+    ContentHeader(text = "Scrobble")
+  }
+}
+
+@Preview
+@ShowkaseComposable(name = "ContentHeader", group = "Common", styleName = "Midnight")
+@Composable
+fun ContentHeaderMidnightPreview() {
+  SunsetThemePreview(theme = AppTheme.MIDNIGHT) {
+    ContentHeader(text = "Scrobble")
+  }
+}
+
+@Preview
+@ShowkaseComposable(name = "ContentHeader", group = "Common", styleName = "Last.fm Dark")
+@Composable
+fun ContentHeaderLastFmDarkPreview() {
+  SunsetThemePreview(theme = AppTheme.LASTFM_DARK) {
+    ContentHeader(text = "Scrobble")
   }
 }
