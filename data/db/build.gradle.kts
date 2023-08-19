a7084c8bf52564cc7c71b7fdf926dd2a7d40ad1b
Takuma Homma <nagomimatcha@gmail.com>
2023-08-19 23:38:09 +0900

    Update kotlin

--- data/db/build.gradle.kts
+++ data/db/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("sunsetscrob.android.feature")
   id("dagger.hilt.android.plugin")
   id("com.google.dagger.hilt.android")
+  id("com.google.devtools.ksp")
 }
 
 android {
@@ -16,7 +17,7 @@ dependencies {
 
   implementation(libs.room.runtime)
   implementation(libs.room.ktx)
-  kapt(libs.room.compiler)
+  ksp(libs.room.compiler)
 
   implementation(libs.datastore.preferences)
 }
