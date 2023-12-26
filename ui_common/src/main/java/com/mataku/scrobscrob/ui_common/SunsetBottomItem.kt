cac2118f6b92b537342ca1e975b807ceaf46b05b
Takuma Homma <nagomimatcha@gmail.com>
2023-12-26 22:23:28 +0900

    Add blur helper

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/SunsetBottomItem.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/SunsetBottomItem.kt
@@ -1,30 +1,41 @@
 package com.mataku.scrobscrob.ui_common
 
 import androidx.annotation.DrawableRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.LibraryMusic
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
 
+// NOTE: merge iconDrawable into ImageVector if possible
 enum class SunsetBottomNavItem(
   @DrawableRes val iconDrawable: Int?,
   val title: String,
-  val screenRoute: String
+  val screenRoute: String,
+  val icon: ImageVector?
 ) {
   SCROBBLE(
     R.drawable.ic_last_fm_logo,
     "Scrobble",
-    "scrobble"
+    "scrobble",
+    null
   ),
   TOP_ALBUMS(
     R.drawable.ic_album_black_24px,
     "Top Albums",
-    "top_albums"
+    "top_albums",
+    Icons.Default.LibraryMusic
   ),
   TOP_ARTISTS(
     R.drawable.ic_account_circle_black,
     "Top Artists",
-    "top_artists"
+    "top_artists",
+    Icons.Default.AccountCircle
   ),
   ACCOUNT(
     null,
     "Account",
-    "account"
+    "account",
+    Icons.Default.Settings
   );
 }
--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/organism/SunsetNavigationBar.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/organism/SunsetNavigationBar.kt
@@ -1,5 +1,17 @@
 package com.mataku.scrobscrob.ui_common.organism
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
@@ -13,8 +25,13 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -28,6 +45,8 @@ import com.mataku.scrobscrob.ui_common.SunsetBottomNavItem
 import com.mataku.scrobscrob.ui_common.style.LocalAppTheme
 import com.mataku.scrobscrob.ui_common.style.SunsetThemePreview
 import com.mataku.scrobscrob.ui_common.style.accentColor
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.hazeChild
 
 @Composable
 fun SunsetNavigationBar(
@@ -36,96 +55,133 @@ fun SunsetNavigationBar(
   navigateToTopAlbums: () -> Unit,
   navigateToTopArtists: () -> Unit,
   navigateToAccount: () -> Unit,
-
-  ) {
+) {
   val backStackEntry = navController.currentBackStackEntryAsState()
   val route = backStackEntry.value?.destination?.route
   val navItems = remember {
-    SunsetBottomNavItem.values()
+    SunsetBottomNavItem.entries
   }
-  NavigationBar(
-    containerColor = MaterialTheme.colorScheme.primary
+  val hazeState = remember {
+    HazeState()
+  }
+  Box(
+    modifier = Modifier
+      .padding(vertical = 24.dp, horizontal = 48.dp)
+      .fillMaxWidth()
+      .height(64.dp)
+      .hazeChild(state = hazeState)
   ) {
-    navItems.forEach { item ->
-      val selected = item.screenRoute == route
+    val selectedItem =
+      SunsetBottomNavItem.entries.find { it.screenRoute == route } ?: SunsetBottomNavItem.SCROBBLE
+    BottomBarTabs(
+      tabs = SunsetBottomNavItem.entries,
+      selectedItem = selectedItem,
+      onTabSelected = { item ->
+        if (item == selectedItem) return@BottomBarTabs
 
-      val iconColor = if (selected) {
-        LocalAppTheme.current.accentColor()
-      } else {
-        MaterialTheme.colorScheme.onPrimary.copy(
-          alpha = 0.4F
-        )
-      }
-      NavigationBarItem(
-        selected = selected,
-        onClick = {
-          if (selected) return@NavigationBarItem
-
-          when (item) {
-            SunsetBottomNavItem.SCROBBLE -> {
-              navigateToScrobble.invoke()
-            }
-
-            SunsetBottomNavItem.ACCOUNT -> {
-              navigateToAccount.invoke()
-            }
-
-            SunsetBottomNavItem.TOP_ARTISTS -> {
-              navigateToTopArtists.invoke()
-            }
-
-            SunsetBottomNavItem.TOP_ALBUMS -> {
-              navigateToTopAlbums.invoke()
-            }
+        when (item) {
+          SunsetBottomNavItem.SCROBBLE -> {
+            navigateToScrobble.invoke()
           }
-        },
-        label = {
-          Text(
-            text = item.title,
-            fontWeight = FontWeight.SemiBold
-          )
-        },
-        icon = {
-          when (item) {
-            SunsetBottomNavItem.SCROBBLE -> {
-              Icon(
-                painterResource(item.iconDrawable!!),
-                contentDescription = item.title,
-                modifier = Modifier
-                  .size(23.dp)
-                  .padding(bottom = 1.dp),
-                tint = iconColor
-              )
-            }
 
-            SunsetBottomNavItem.TOP_ALBUMS -> {
-              Icon(
-                imageVector = Icons.Default.LibraryMusic,
-                contentDescription = item.title,
-                tint = iconColor
-              )
-            }
+          SunsetBottomNavItem.ACCOUNT -> {
+            navigateToAccount.invoke()
+          }
 
-            SunsetBottomNavItem.TOP_ARTISTS -> {
-              Icon(
-                imageVector = Icons.Default.AccountCircle,
-                contentDescription = item.title,
-                tint = iconColor
-              )
-            }
+          SunsetBottomNavItem.TOP_ARTISTS -> {
+            navigateToTopArtists.invoke()
+          }
 
-            SunsetBottomNavItem.ACCOUNT -> {
-              Icon(
-                imageVector = Icons.Default.Settings,
-                contentDescription = item.title,
-                tint = iconColor
-              )
-            }
+          SunsetBottomNavItem.TOP_ALBUMS -> {
+            navigateToTopAlbums.invoke()
           }
         }
-      )
-    }
+      })
   }
+
+//  NavigationBar(
+//    containerColor = MaterialTheme.colorScheme.primary
+//  ) {
+//    navItems.forEach { item ->
+//      val selected = item.screenRoute == route
+//
+//      val iconColor = if (selected) {
+//        LocalAppTheme.current.accentColor()
+//      } else {
+//        MaterialTheme.colorScheme.onPrimary.copy(
+//          alpha = 0.4F
+//        )
+//      }
+//      NavigationBarItem(
+//        selected = selected,
+//        onClick = {
+//          if (selected) return@NavigationBarItem
+//
+//          when (item) {
+//            SunsetBottomNavItem.SCROBBLE -> {
+//              navigateToScrobble.invoke()
+//            }
+//
+//            SunsetBottomNavItem.ACCOUNT -> {
+//              navigateToAccount.invoke()
+//            }
+//
+//            SunsetBottomNavItem.TOP_ARTISTS -> {
+//              navigateToTopArtists.invoke()
+//            }
+//
+//            SunsetBottomNavItem.TOP_ALBUMS -> {
+//              navigateToTopAlbums.invoke()
+//            }
+//          }
+//        },
+//        label = {
+//          Text(
+//            text = item.title,
+//            fontWeight = FontWeight.SemiBold
+//          )
+//        },
+//        icon = {
+//          when (item) {
+//            SunsetBottomNavItem.SCROBBLE -> {
+//              Icon(
+//                painterResource(item.iconDrawable!!),
+//                contentDescription = item.title,
+//                modifier = Modifier
+//                  .size(23.dp)
+//                  .padding(bottom = 1.dp),
+//                tint = iconColor
+//              )
+//            }
+//
+//            SunsetBottomNavItem.TOP_ALBUMS -> {
+//              Icon(
+//                imageVector = Icons.Default.LibraryMusic,
+//                contentDescription = item.title,
+//                tint = iconColor
+//              )
+//            }
+//
+//            SunsetBottomNavItem.TOP_ARTISTS -> {
+//              Icon(
+//                imageVector = Icons.Default.AccountCircle,
+//                contentDescription = item.title,
+//                tint = iconColor
+//              )
+//            }
+//
+//            SunsetBottomNavItem.ACCOUNT -> {
+//              Icon(
+//                imageVector = Icons.Default.Settings,
+//                contentDescription = item.title,
+//                tint = iconColor
+//              )
+//            }
+//          }
+//        }
+//      )
+//    }
+//  }
 }
 
 @Preview
@@ -158,6 +214,45 @@ fun SunsetNavigationBarLightPreview() {
         navigateToTopAlbums = {},
         navigateToTopArtists = {}
       )
+    }
+  }
+}
+
+@Composable
+private fun BottomBarTabs(
+  tabs: List<SunsetBottomNavItem>,
+  selectedItem: SunsetBottomNavItem,
+  onTabSelected: (SunsetBottomNavItem) -> Unit
+) {
+  Row(
+    modifier = Modifier.fillMaxSize(),
+  ) {
+    tabs.forEach { tab ->
+      val isSelectedTab = tab == selectedItem
+      val alpha by animateFloatAsState(
+        targetValue = if (isSelectedTab) 1F else 0.4F,
+        label = "alpha"
+      )
+      Column(
+        modifier = Modifier
+          .alpha(alpha)
+          .fillMaxHeight()
+          .weight(1f)
+          .pointerInput(Unit) {
+            detectTapGestures {
+              onTabSelected(tab)
+            }
+          },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+      ) {
+        if (tab == SunsetBottomNavItem.SCROBBLE) {
+          Icon(painterResource(id = tab.iconDrawable!!), contentDescription = "tab ${tab.title}")
+        } else {
+          Icon(imageVector = tab.icon!!, contentDescription = "tab ${tab.title}")
+        }
+        Text(text = tab.title)
+      }
     }
   }
 }
