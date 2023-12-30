9b4bf53d1cf15072d23a3f8520f051e6e0ef22c9
Takuma Homma <nagomimatcha@gmail.com>
2023-12-31 01:21:36 +0900

    Try

--- benchmark/src/main/java/com/example/benchmark/BaselineProfileGenerator.kt
+++ benchmark/src/main/java/com/example/benchmark/BaselineProfileGenerator.kt
@@ -1,11 +1,13 @@
 package com.example.benchmark
 
+import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@OptIn(ExperimentalBaselineProfilesApi::class)
 @RunWith(AndroidJUnit4::class)
 class BaselineProfileGenerator {
   @get:Rule
@@ -13,10 +15,8 @@ class BaselineProfileGenerator {
 
   @Test
   fun generateBaselineProfile() {
-    baselineProfileRule.collect(packageName = "com.mataku.scrobscrob") {
-      pressHome()
+    baselineProfileRule.collectBaselineProfile(packageName = "com.mataku.scrobscrob") {
       startActivityAndWait()
-      device.waitForIdle()
     }
   }
 }
