afb8d23c424887cbb8cf4a5608668790697dccb4
Takuma Homma <nagomimatcha@gmail.com>
2024-01-02 21:12:54 +0900

    Adjust cell style

--- feature/album/src/main/java/com/mataku/scrobscrob/album/ui/molecule/TopAlbum.kt
+++ feature/album/src/main/java/com/mataku/scrobscrob/album/ui/molecule/TopAlbum.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -70,7 +71,9 @@ fun TopAlbum(
       album.title,
       maxLines = 1,
       overflow = TextOverflow.Ellipsis,
-      style = SunsetTextStyle.body
+      style = SunsetTextStyle.body.copy(
+        fontWeight = FontWeight.Bold
+      )
     )
     Text(
       album.artist,
