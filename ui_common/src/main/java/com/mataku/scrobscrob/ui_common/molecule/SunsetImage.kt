549ade4e04fa7c73f339356e21516032342b0de3
Takuma Homma <nagomimatcha@gmail.com>
2024-01-02 20:44:30 +0900

    Consider preview Image

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/molecule/SunsetImage.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/molecule/SunsetImage.kt
@@ -1,20 +1,33 @@
 package com.mataku.scrobscrob.ui_common.molecule
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 
 @Composable
 fun SunsetImage(
+  modifier: Modifier = Modifier,
   imageData: Any?,
   contentDescription: String?,
   size: Int? = null,
   contentScale: ContentScale = ContentScale.Fit,
-  modifier: Modifier
 ) {
+  if (LocalInspectionMode.current) {
+    Image(
+      painter = painterResource(id = com.mataku.scrobscrob.ui_common.R.drawable.no_image),
+      contentDescription = "",
+      modifier = modifier,
+      contentScale = contentScale
+    )
+    return
+  }
+
   val imageRequestBuilder = ImageRequest.Builder(LocalContext.current)
     .data(imageData)
   if (size != null) {
