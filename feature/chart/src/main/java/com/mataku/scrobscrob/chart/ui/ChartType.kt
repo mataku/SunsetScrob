2089fa775608c6c772e61665b62891e5852ad5c2
Takuma Homma <nagomimatcha@gmail.com>
2024-01-02 20:44:18 +0900

    Prepare ChartScreen

--- feature/chart/src/main/java/com/mataku/scrobscrob/chart/ui/ChartType.kt
+++ feature/chart/src/main/java/com/mataku/scrobscrob/chart/ui/ChartType.kt
@@ -3,7 +3,13 @@ package com.mataku.scrobscrob.chart.ui
 import androidx.compose.runtime.Immutable
 
 @Immutable
-enum class ChartType {
-  TOP_ARTISTS,
-  TOP_TRACKS,
+enum class ChartType(val index: Int, val tabName: String) {
+  TOP_ARTISTS(0, "Artist"),
+  TOP_TRACKS(1, "Track");
+
+  companion object {
+    fun findByIndex(index: Int): ChartType {
+      return ChartType.entries.find { it.index == index } ?: TOP_ARTISTS
+    }
+  }
 }
