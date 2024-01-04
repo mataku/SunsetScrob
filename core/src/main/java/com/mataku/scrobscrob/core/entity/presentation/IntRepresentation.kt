21a7df04d7205b4eb4e2b95403e62d1024724f62
Takuma Homma <nagomimatcha@gmail.com>
2024-01-05 01:09:35 +0900

    Add parser for Int value representation

---
+++ core/src/main/java/com/mataku/scrobscrob/core/entity/presentation/IntRepresentation.kt
@@ -0,0 +1,14 @@
+package com.mataku.scrobscrob.core.entity.presentation
+
+import android.icu.text.CompactDecimalFormat
+import java.util.Locale
+
+fun String.toReadableIntValue(): String {
+  return runCatching {
+    val compactDecimalFormat = CompactDecimalFormat.getInstance(
+      Locale.ENGLISH,
+      CompactDecimalFormat.CompactStyle.SHORT
+    )
+    compactDecimalFormat.format(this.toInt())
+  }.getOrNull() ?: this
+}
