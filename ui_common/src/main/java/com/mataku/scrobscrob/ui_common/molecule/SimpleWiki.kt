b3856315e23108309f873364ea1b7e420cc39d9a
Takuma Homma <nagomimatcha@gmail.com>
2024-01-05 23:04:28 +0900

    Cleanup unused imports

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/molecule/SimpleWiki.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/molecule/SimpleWiki.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -56,8 +55,7 @@ fun SimpleWiki(
           role = null
         ) {
           onUrlTap.invoke(url)
-        }
-      ,
+        },
       textAlign = TextAlign.Center
     )
   }
