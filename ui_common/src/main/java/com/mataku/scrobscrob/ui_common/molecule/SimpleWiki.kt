ef8dbfe2777850c48e1d1f7b8d2e39c7aaf068ea
Takuma Homma <nagomimatcha@gmail.com>
2024-01-05 22:52:08 +0900

    no ripple

--- ui_common/src/main/java/com/mataku/scrobscrob/ui_common/molecule/SimpleWiki.kt
+++ ui_common/src/main/java/com/mataku/scrobscrob/ui_common/molecule/SimpleWiki.kt
@@ -1,6 +1,7 @@
 package com.mataku.scrobscrob.ui_common.molecule
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,9 +48,16 @@ fun SimpleWiki(
         color = LocalAppTheme.current.accentColor()
       ),
       modifier = Modifier
-        .clickable {
+        .clickable(
+          interactionSource = remember { MutableInteractionSource() },
+          indication = null,
+          enabled = true,
+          onClickLabel = "Read $name more click",
+          role = null
+        ) {
           onUrlTap.invoke(url)
-        },
+        }
+      ,
       textAlign = TextAlign.Center
     )
   }
