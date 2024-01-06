f0115f888aa6a39846d769f9666ccc72af75dee9
Takuma Homma <nagomimatcha@gmail.com>
2024-01-07 01:17:17 +0900

    Cleanup

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,19 +11,9 @@ android {
 dependencies {
   implementation(libs.ktor.client.okhttp)
   implementation(libs.ktor.client.logging)
-  implementation(platform(libs.okhttp.bom))
-  implementation(libs.okhttp)
-  implementation(libs.okhttp.logging.interceptor)
-  implementation(libs.serialization.json)
   implementation(libs.coroutines)
 
   implementation(platform(libs.compose.bom))
   implementation(libs.compose.runtime)
   implementation(libs.kotlinx.collection)
-}
-
-repositories {
-  google()
-  mavenCentral()
-  maven("https://maven.google.com")
 }
