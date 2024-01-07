704ba1007943f139a5479660a8b0ab29b585e207
Takuma Homma <nagomimatcha@gmail.com>
2024-01-07 21:47:54 +0900

    Add ArtistScreen

--- feature/artist/src/main/java/com/mataku/scrobscrob/artist/ui/navigation/ArtistNavigation.kt
+++ feature/artist/src/main/java/com/mataku/scrobscrob/artist/ui/navigation/ArtistNavigation.kt
@@ -4,16 +4,43 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import com.mataku.scrobscrob.artist.ui.screen.ArtistScreen
 import com.mataku.scrobscrob.artist.ui.screen.TopArtistsScreen
+import com.mataku.scrobscrob.core.entity.imageUrl
+import com.mataku.scrobscrob.ui_common.navigateToWebView
 
 fun NavGraphBuilder.artistGraph(navController: NavController) {
   navigation(route = ARTIST_NAVIGATION_ROUTE, startDestination = TOP_ARTISTS_DESTINATION) {
     composable(TOP_ARTISTS_DESTINATION) {
       TopArtistsScreen(
         viewModel = hiltViewModel(),
-        navController = navController
+        onArtistTap = {
+          navController.navigateToArtistInfo(
+            artistName = it.name,
+            artworkUrl = it.imageList.imageUrl() ?: "",
+          )
+        }
+      )
+    }
+
+    composable(
+      "${ARTIST_INFO_DESTINATION}?${ARTIST_NAME_ARGUMENT}={artistName}&${ARTWORK_URL_ARGUMENT}={artworkUrl}",
+      arguments = listOf(
+        navArgument(ARTIST_NAME_ARGUMENT) {
+          type = NavType.StringType
+        },
+        navArgument(ARTWORK_URL_ARGUMENT) {
+          type = NavType.StringType
+        }
+      )
+    ) {
+      ArtistScreen(
+        viewModel = hiltViewModel(),
+        onArtistLoadMoreTap = navController::navigateToWebView
       )
     }
   }
@@ -29,5 +56,18 @@ fun NavController.navigateToTopArtists() {
   }
 }
 
+fun NavController.navigateToArtistInfo(
+  artistName: String,
+  artworkUrl: String
+) {
+  val destination =
+    "${ARTIST_INFO_DESTINATION}?${ARTIST_NAME_ARGUMENT}=${artistName}&${ARTWORK_URL_ARGUMENT}=${artworkUrl}"
+  navigate(destination)
+}
+
+private const val ARTIST_NAME_ARGUMENT = "artistName"
+private const val ARTWORK_URL_ARGUMENT = "artworkUrl"
+
+private const val ARTIST_INFO_DESTINATION = "top_artists"
 private const val TOP_ARTISTS_DESTINATION = "top_artists"
 private const val ARTIST_NAVIGATION_ROUTE = "artist_route"
