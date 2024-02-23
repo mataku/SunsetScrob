0e833f3bd7c1a3627625c558c5337bf96751b9a6
Takuma Homma <nagomimatcha@gmail.com>
2024-02-23 23:29:09 +0900

    Compare directly

--- test_helper/integration/src/main/java/com/mataku/scrobscrob/test_helper/integration/ComposeRuleExtension.kt
+++ test_helper/integration/src/main/java/com/mataku/scrobscrob/test_helper/integration/ComposeRuleExtension.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.isRoot
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
-import com.github.takahirom.roborazzi.RoborazziOptions
 import com.github.takahirom.roborazzi.captureRoboImage
 import com.mataku.scrobscrob.core.entity.AppTheme
 import com.mataku.scrobscrob.ui_common.style.SunsetThemePreview
@@ -29,8 +28,5 @@ fun ComposeContentTestRule.captureScreenshot(
 
   this.onNode(isRoot()).captureRoboImage(
     filePath = "screenshot/${fileName}",
-    roborazziOptions = RoborazziOptions(
-      compareOptions = RoborazziOptions.CompareOptions(changeThreshold = 0.01f)
-    )
   )
 }
