e637f19d9550502d04b162eca91940e38dc2b298
Takuma Homma <nagomimatcha@gmail.com>
2024-05-05 00:23:46 +0900

    Add home module

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+  id("sunsetscrob.android.feature")
+  id("sunsetscrob.android.compose")
+  id("sunsetscrob.android.dagger")
+  id("sunsetscrob.android.test.screenshot")
+}
+
+android {
+  namespace = "com.mataku.scrobscrob.home"
+}
+
+dependencies {
+}
