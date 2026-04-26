5efa80b97a58cf1c7fbb7b9537f336219fda6de0
Takuma Homma <nagomimatcha@gmail.com>
2026-04-26 20:33:19 +0900

    Adjust bottom space for LicenseScreen with no bottom nav

--- feature/account/src/main/java/com/mataku/scrobscrob/account/ui/screen/LicenseScreen.kt
+++ feature/account/src/main/java/com/mataku/scrobscrob/account/ui/screen/LicenseScreen.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -47,6 +50,7 @@ internal fun LicenseScreen(
         SunsetHorizontalDivider()
       }
     },
+    contentPadding = WindowInsets.navigationBars.asPaddingValues(),
     modifier = modifier
       .fillMaxSize()
       .padding(
