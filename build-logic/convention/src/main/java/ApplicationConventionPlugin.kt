c2b53d7de4590475796164de15485b510137578e
Takuma Homma <nagomimatcha@gmail.com>
2026-05-15 23:38:51 +0900

    Bump

--- build-logic/convention/src/main/java/ApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/ApplicationConventionPlugin.kt
@@ -9,7 +9,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
 class ApplicationConventionPlugin : Plugin<Project> {
-  private val appVersionName = "1.27.0"
+  private val appVersionName = "1.28.0"
 
   override fun apply(target: Project) {
     with(target) {
